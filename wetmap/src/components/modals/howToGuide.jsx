926b359d11a7b4dc301e287b7cc1e7beb61015b1
Freem11 <freem1985@gmail.com>
2023-12-19 05:11:25 -0700

    guide buttons prepared

--- wetmap/src/components/modals/howToGuide.jsx
+++ wetmap/src/components/modals/howToGuide.jsx
@@ -18,13 +18,13 @@ const HowToGuide = (props) => {
     <div className="masterDiv">
       <div className="topRead">
         <div className="titleDiv">
-          <h3 style={{ marginTop: 3, marginRight: 85, width: "200%", fontFamily: "Patrick Hand", fontSize: 32}}>How to use Scuba SEAsons</h3>
+          <h3 style={{ marginTop: 3, marginRight: 45, width: "200%", fontFamily: "Patrick Hand", fontSize: 32}}>How to use Scuba SEAsons</h3>
           <FormGroup>
             <Button
               variant="text"
               id="closeButton2"
               onClick={() => animateLaunchModal()}
-              style={{ display: "flex", flexDirection: "column", marginRight: 40, marginTop: 10, backgroundColor: "transparent", border: "none"}}
+              style={{ display: "flex", flexDirection: "column", marginRight: 20, marginTop: 10, backgroundColor: "transparent", border: "none"}}
             >
               <CloseIcon
                 sx={{ color: "lightgrey", height: "36px", width: "36px" }}
@@ -35,7 +35,47 @@ const HowToGuide = (props) => {
 
         <div className="mainBlurbDiv">
      
-        <h1>HI</h1>
+        <div onClick={null} className="introGuideLaunch">
+          <Label
+            style={{
+              fontFamily: "Itim",
+              fontWeight: "bold",
+              color: "gold",
+              cursor: "pointer",
+              marginTop: "5px",
+            }}
+          >
+            Intro Guide
+          </Label>
+        </div>
+
+        <div onClick={null} className="diveSiteGuideLaunch">
+          <Label
+            style={{
+              fontFamily: "Itim",
+              fontWeight: "bold",
+              color: "gold",
+              cursor: "pointer",
+              marginTop: "5px",
+            }}
+          >
+            Fun With Dive Sites
+          </Label>
+        </div>
+
+        <div onClick={null} className="photoGuideLaunch">
+          <Label
+            style={{
+              fontFamily: "Itim",
+              fontWeight: "bold",
+              color: "gold",
+              cursor: "pointer",
+              marginTop: "5px",
+            }}
+          >
+            Photogenics
+          </Label>
+        </div>
 
         </div>
       </div>
