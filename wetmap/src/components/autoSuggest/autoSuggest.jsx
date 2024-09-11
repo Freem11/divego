c9ba969a859222a37e28b7a9ea7a50f7fd9cc404
Nataliia Dovha <natadnepr027@gmail.com>
2024-09-11 17:15:17 -0600

    fixed large buttons and map zoom

--- wetmap/src/components/autoSuggest/autoSuggest.jsx
+++ wetmap/src/components/autoSuggest/autoSuggest.jsx
@@ -1,55 +1,80 @@
-import React from "react";
-import AutoSuggestListItem from "./autoSuggestListItem";
-import HighlightOffIcon from "@mui/icons-material/HighlightOff";
-import "./autoSuggest.css";
-import InputField from "../reusables/inputField";
+import React from 'react';
+import AutoSuggestListItem from './autoSuggestListItem';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import SearchIcon from '@mui/icons-material/Search';
+import './autoSuggest.css';
+import InputField from '../reusables/inputField';
 
 export default function AutoSuggest(props) {
-  const { placeholder, value, list, clear, handleSelect, handleChange, style, style1, style2, style3 } = props;
+	const {
+		placeholder,
+		value,
+		list,
+		clear,
+		handleSelect,
+		handleChange,
+		style,
+		style1,
+		style2,
+		style3,
+	} = props;
 
-  return (
-    <div>
-      <InputField
+	return (
+		<div className='column col-11'>
+			<InputField
         className="suggestInput"
         placeholder={placeholder}
         name={placeholder}
         value={value}
         onChange={handleChange}
       />
+			{/* <div className='relative flex-row'>
+				<div className='absolute z-10 left-3 top-1/2 transform -translate-y-1/2 text-gray-400'>
+					<SearchIcon  />
+				</div>
 
-      {value.length > 1 && (
-        <div variant="text" id="XButton" onClick={clear}>
-          <HighlightOffIcon
-            sx={{
-              color: "white",
-              height: "2vh",
-              width: "2vw",
-              position: "absolute",
-            }}
-          ></HighlightOffIcon>
-        </div>
-      )}
+				<input
+					type='search'
+					className='form-input pl-10' // Adjust padding to make space for the icon
+					value={value}
+					onChange={handleChange}
+					placeholder='Search Places...'
+				/>
+			</div> */}
+			{value.length > 1 && (
+				<div variant='text' id='XButton' onClick={clear}>
+					<HighlightOffIcon
+						sx={{
+							color: 'white',
+							height: '2vh',
+							width: '2vw',
+							position: 'absolute',
+						}}
+					></HighlightOffIcon>
+				</div>
+			)}
 
-      <div
-        style={{...style1,
-          height: "auto",
-          zIndex: "100",
-          position: "absolute",
-        }}
-      >
-        {list.length > 0 &&
-          list.map((element) => {
-            return (
-              <AutoSuggestListItem
-                key={element}
-                value={element}
-                handleSelect={handleSelect}
-                style={style2}
-                style3={style3}
-              />
-            );
-          })}
-      </div>
-    </div>
-  );
+			<div
+				style={{
+					...style1,
+					height: 'auto',
+					zIndex: '100',
+					position: 'absolute',
+				}}
+			>
+				{list.length > 0 &&
+					list.map((element) => {
+						return (
+							<AutoSuggestListItem
+								key={element}
+								value={element}
+								handleSelect={handleSelect}
+								style={style2}
+								style3={style3}
+							/>
+						);
+					})}
+			</div>
+		</div>
+	);
 }
--- wetmap/src/components/contexts/mapZoomContext.jsx
+++ wetmap/src/components/contexts/mapZoomContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const ZoomContext = createContext('');
 
 const ZoomContextProvider = ({children}) => {
-    const [mapZoom, setMapZoom] = useState(10);
+    const [mapZoom, setMapZoom] = useState(15);
 
     return (
         <ZoomContext.Provider value={{ mapZoom, setMapZoom }}>
