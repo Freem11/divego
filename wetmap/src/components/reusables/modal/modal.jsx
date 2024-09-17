2626845cdaf2a3709c130fc39aed4e1ee04de424
taras <tymchenko.dev@gmail.com>
2024-09-16 18:56:29 -0600

    WEB-11 add different sizes

--- wetmap/src/components/reusables/modal/modal.jsx
+++ wetmap/src/components/reusables/modal/modal.jsx
@@ -1,51 +1,78 @@
-import React, {useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { ModalContext } from "../../contexts/modalContext";
-import style from './modal.module.scss';
+import style from "./modal.module.scss";
 
 export default function Modal(props) {
+    const rootRef = useRef(null);
+    const modalContext = useContext(ModalContext);
 
-  const rootRef = useRef(null);
-  const modalContext = useContext(ModalContext);
+    useEffect(() => {
+        const handleWrapperClick = (e) => {
+            if (!modalContext.stack?.length) {
+                // not need to close modal if none is open
+                return;
+            }
 
+            if (rootRef.current.contains(e.target)) {
+                // no need to close modal if click inside modal wrapper
+                return;
+            }
 
-  useEffect(() => {
-    const handleWrapperClick = (e) => {
-      if(!modalContext.stack?.length){
-        // not need to close modal if none is open
-        return
-      }
+            // close modal if click outside of modal wrapper
+            modalContext.modalCancel();
+        };
 
-      if (rootRef.current.contains(e.target)){
-        // no need to close modal if click inside modal wrapper
-        return
-      }
+        const handleEscapePress = (event) => {
+            if (event.key === "Escape") {
+                modalContext.modalCancel();
+            }
+        };
 
-      // close popul if click outside of modal wrapper
-      modalContext.modalClose()
+        window.addEventListener("click", handleWrapperClick);
+        window.addEventListener("keydown", handleEscapePress);
+        return () => {
+            window.removeEventListener("click", handleWrapperClick);
+            window.removeEventListener("keydown", handleEscapePress);
+        };
+    }, [modalContext]);
+
+    if (!modalContext.stack?.length) {
+        return <div className={`${style.modalWrapper}`} ref={rootRef}></div>;
     }
 
-    const handleEscapePress = (event) => {
-      if (event.key === 'Escape') {
-        modalContext.modalClose();
-      }
+    const openStyle = {
+        transform: "translateY(0)",
+        transition: `transform ${modalContext.modalAnimationDuration}ms ease-in-out`,
     };
 
-    window.addEventListener('click', handleWrapperClick)
-    window.addEventListener('keydown', handleEscapePress)
-    return () => {
-      window.removeEventListener('click', handleWrapperClick)
-      window.removeEventListener('keydown', handleEscapePress)
+    const closeStyle = {
+        transition: `transform ${modalContext.modalAnimationDuration}ms ease-out`,
     };
-  }, [modalContext]);
 
-
-
-  return <div className={`${style.modalWrapper} ${modalContext.stack?.length ? style.active : ""}`} ref={rootRef}>
-        {modalContext.stack?.map((modal) => <div className={`${style.modalContainer} ${modal.component.name === modalContext.getCurrentModalName() ? style.open : ""}`} key={modal.component.name}>
-          {<modal.component 
-            onFinish={() => {modalContext.modalClose()}}
-          />}
-          </div>
-        )}
-  </div>
+    
+ 
+ 
+    return (
+        <div className={`${style.modalWrapper} ${style.active}`} ref={rootRef}>
+            {modalContext.stack.map((modalWindow) => {
+                
+                return <div
+                    className={`${style.modalContainer} ${style[modalWindow.options.size]}`}
+                    style={modalWindow.name === modalContext.currentModalName ? openStyle : closeStyle}
+                    key={modalWindow.name}
+                >
+                    {
+                        <modalWindow.component
+                            onModalSuccess={() => {
+                                modalContext.modalSuccess();
+                            }}
+                            onModalCancel={() => {
+                                modalContext.modalCancel();
+                            }}
+                        />
+                    }
+                </div>
+            })}
+        </div>
+    );
 }
