61035eeef3772b8d773663bb87b31af92b8c6e49
taras <tymchenko.dev@gmail.com>
2024-10-12 17:14:31 -0600

    eslint config

--- wetmap/eslint.config.mjs
+++ wetmap/eslint.config.mjs
@@ -2,20 +2,15 @@ import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import globals from 'globals';
 import stylistic from '@stylistic/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
+
 
 export default [
-  {
-    files:           ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser:        typescriptParser,
-      parserOptions: {
-        project:         ['./tsconfig.json'],
-        projectService:  true,
-        tsconfigRootDir: '.',
-      },
-    },
-  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  stylistic.configs['recommended-flat'],
+  tseslint.configs.base,
+
   {
     files:    ['**/*.{js,ts,mjs,cjs,jsx,tsx}'],
     settings: {
@@ -30,21 +25,57 @@ export default [
     },
   },
 
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  stylistic.configs['recommended-flat'],
   {
     rules: {
+      'no-unused-vars': ['error', {
+        'destructuredArrayIgnorePattern': '^_',
+      }],
+
       'react/react-in-jsx-scope': ['off'],
+      'react/prop-types':         ['off'],
 
       '@stylistic/semi':                    ['error', 'always'],
-      '@stylistic/key-spacing':             ['error', { align: 'value' }],
+      '@stylistic/key-spacing':             ['error', { align: 'value', mode: 'minimum' }],
+      '@stylistic/quote-props':             ['error', 'consistent'],
+      '@stylistic/no-multi-spaces':         ['off'],
       '@stylistic/no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
+    },
+  },
+
+  {
+    name:  'typescript',
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    rules: {
+      ...tseslint.configs.eslintRecommended.rules,
+      '@typescript-eslint/ban-ts-comment':                      'error',
+      'no-array-constructor':                                   'off',
+      '@typescript-eslint/no-array-constructor':                'error',
+      '@typescript-eslint/no-duplicate-enum-values':            'error',
+      '@typescript-eslint/no-empty-object-type':                'error',
+      '@typescript-eslint/no-explicit-any':                     'error',
+      '@typescript-eslint/no-extra-non-null-assertion':         'error',
+      '@typescript-eslint/no-misused-new':                      'error',
+      '@typescript-eslint/no-namespace':                        'error',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+      '@typescript-eslint/no-require-imports':                  'error',
+      '@typescript-eslint/no-this-alias':                       'error',
+      '@typescript-eslint/no-unnecessary-type-constraint':      'error',
+      '@typescript-eslint/no-unsafe-declaration-merging':       'error',
+      '@typescript-eslint/no-unsafe-function-type':             'error',
+      'no-unused-expressions':                                  'off',
+      '@typescript-eslint/no-unused-expressions':               'error',
+      'no-unused-vars':                                         'off',
+      '@typescript-eslint/no-unused-vars':                      'error',
+      '@typescript-eslint/no-wrapper-object-types':             'error',
+      '@typescript-eslint/prefer-as-const':                     'error',
+      '@typescript-eslint/prefer-namespace-keyword':            'error',
+      '@typescript-eslint/triple-slash-reference':              'error',
     },
   },
 ];
 
-// recommended rules from eslint is added to main config above (stylistic.configs['recommended-flat'])
+
+// recommended format rules from eslint "stylistic" plugin is added to main config above (stylistic.configs['recommended-flat'])
 // and printed below just to make it easier to understand what rules are currently in use and how to override them
 // severities: off | warning | error
 /*
