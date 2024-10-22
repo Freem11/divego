eb4034c424dc906cca829c73e7fe0431b0ef8f8f
Freem11 <freem1985@gmail.com>
2024-10-21 22:25:13 -0600

    dive site modal resized and placeholder picture in place

--- wetmap/src/components/newModals/diveSite.jsx
+++ wetmap/src/components/newModals/diveSite.jsx
@@ -1,423 +1,226 @@
-import React, {
-  useState,
-  useContext,
-  useEffect,
-} from "react";
-import WavyHeaderDynamic from "./wavyHeaderDynamic";
-import PlainTextInput from "./plaintextInput";
-// import {
-//   activeFonts,
-//   colors,
-//   fontSizes,
-//   screenSecondaryButton,
-//   buttonTextAlt,
-// } from "../styles";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import screenData from "./screenData.json";
+import style from "./modalContent.module.scss";
+import LargeButton from "./largeButton";
+import Button from "./button";
+import { FormGroup, Input } from "reactstrap";
+import WavyHeader from "./wavyHeader";
+import TextInputField from "../newModals/textInput";
 import { PinContext } from "../contexts/staticPinContext";
 import { UserProfileContext } from "../contexts/userProfileContext";
-import { SelectedDiveSiteContext } from "../contexts/selectedDiveSiteContext";
-import { AnimalMultiSelectContext } from "../contexts/animalMultiSelectContext";
-import { MyCreaturesContext } from "../contexts/myCreaturesContext";
-import { newGPSBoundaries } from "../helpers/mapHelpers";
-import { chooseImageHandler } from "./imageUploadHelpers";
-import { useButtonPressHelper } from "../FABMenu/buttonPressHelper";
-import {
-  uploadphoto,
-  removePhoto,
-} from "./../cloudflareBucketCalls/cloudflareAWSCalls";
+import { MaterialIcons } from "react-web-vector-icons";
 import {
   getPhotosWithUser,
   getPhotosWithUserEmpty,
   getPhotosByDiveSiteWithExtra,
 } from "../../supabaseCalls/photoSupabaseCalls";
-import {
-  getDiveSiteWithUserName,
-  updateDiveSite,
-} from "../../supabaseCalls/diveSiteSupabaseCalls";
-import BottomDrawer from "./animatedBottomDrawer";
+import { handleImageUpload, clearPreviousImage } from "./imageUploadHelpers";
+import defaultImage from "../../images/blackManta.png";
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+const screenWidthInital = window.innerWidth;
+const screenHeitghInital = window.innerHeight;
 
 export default function DiveSite(props) {
   const {} = props;
   const { profile } = useContext(UserProfileContext);
-  const { animalMultiSelection } = useContext(AnimalMultiSelectContext);
-  const { myCreatures } = useContext(MyCreaturesContext);
-  const { pinValues, setPinValues } = useContext(PinContext);
-  const { selectedDiveSite } = useContext(SelectedDiveSiteContext);
-  const { levelOneScreen, setLevelOneScreen } = useContext(
-    LevelOneScreenContext
-  );
-  const { levelTwoScreen, setLevelTwoScreen } = useContext(
-    LevelTwoScreenContext
-  );
-
-  const { activeScreen, setActiveScreen } = useContext(ActiveScreenContext);
-  const { setPreviousButtonID } = useContext(PreviousButtonIDContext);
-  const [diveSitePics, setDiveSitePics] = useState([]);
   const [site, setSite] = useState("");
-  const [diveSiteVals, setDiveSiteVals] = useState({
-    diveSitebio: null,
-    diveSitePhoto: null,
-  });
-  const [isEditModeOn, setIsEditModeOn] = useState(false);
-  const [isPartnerAccount, setIsPartnerAccount] = useState(false);
-  const drawerUpperBound = "90%";
-  const drawerLowerBound = "30%";
+  const [picUrl, setPicUrl] = useState(defaultImage);
 
-  const getPhotos = async () => {
-    const success = await getPhotosByDiveSiteWithExtra({
-      lat: selectedDiveSite.Latitude,
-      lng: selectedDiveSite.Longitude,
-      userId: profile[0].UserID,
-    });
-    setDiveSitePics(success);
-  };
+  function handleClick() {
+    document.getElementById("file").click();
+  }
 
-  useEffect(() => {
-    getPhotos();
-  }, []);
+  const handleImageSelection = async (e) => {
+    if (e.target && e.target.name === "picFile") {
+      if (site.photo !== null) {
+        clearPreviousImage(site.photo);
+      }
 
-  useEffect(() => {
-    getPhotos();
-  }, [selectedDiveSite]);
-
-  useEffect(() => {
-    if (!isEditModeOn && site) {
-      diveSiteUpdateUpdate();
-    }
-  }, [isEditModeOn]);
-
-  const diveSiteUpdateUpdate = async () => {
-    try {
-      const success = await updateDiveSite({
-        id: site.id,
-        bio: site.divesitebio,
-        photo: site.divesiteprofilephoto,
+      const createFileName = await handleImageUpload(e);
+      setSite({
+        ...site,
+        photo: `animalphotos/public/${createFileName}`,
       });
-    } catch (e) {
-      console.log({ title: "Error19", message: e.message });
     }
   };
 
   useEffect(() => {
-    getDiveSite(selectedDiveSite.SiteName);
-    // filterAnchorPhotos();
-  }, [selectedDiveSite]);
-
-  useEffect(() => {
-    if(profile[0].partnerAccount){
-      setIsPartnerAccount(true)
+    if (site.photo) {
+      let photoName = site.photo.split("/").pop();
+      setPicUrl(
+        import.meta.env.VITE_CLOUDFLARE_R2_BUCKET_PATH + `${photoName}`
+      );
+    } else {
+      setPicUrl(null);
     }
+  }, [site.photo]);
 
-    getDiveSite(selectedDiveSite.SiteName);
-    // filterAnchorPhotos();
-  }, []);
-
-  const getDiveSite = async () => {
-    try {
-      const selectedSite = await getDiveSiteWithUserName({
-        siteName: selectedDiveSite.SiteName,
-        lat: selectedDiveSite.Latitude,
-        lng: selectedDiveSite.Longitude,
-      });
-      if (selectedSite.length > 0) {
-        setSite(selectedSite[0]);
-      }
-    } catch (e) {
-      console.log({ title: "Error98", message: e.message });
+  const onClose = async () => {
+    if (site.photo !== null || site.photo === "") {
+      await clearPreviousImage(site.photo);
     }
-  };
-
-  const filterAnchorPhotos = async () => {
-    let { minLat, maxLat, minLng, maxLng } = newGPSBoundaries(
-      selectedDiveSite.Latitude,
-      selectedDiveSite.Longitude
-    );
-
-    try {
-      let photos;
-      if (animalMultiSelection.length === 0) {
-        photos = await getPhotosWithUserEmpty({
-          myCreatures,
-          userId: profile[0].UserID,
-          minLat,
-          maxLat,
-          minLng,
-          maxLng,
-        });
-      } else {
-        photos = await getPhotosWithUser({
-          animalMultiSelection,
-          myCreatures,
-          userId: profile[0].UserID,
-          minLat,
-          maxLat,
-          minLng,
-          maxLng,
-        });
-      }
-
-      if (photos) {
-        // photos.unshift({ id: 0 });
-        setDiveSitePics(photos);
-        let count = 0;
-        photos.forEach((obj) => {
-          count++;
-        });
-      }
-    } catch (e) {
-      console.log({ title: "Error55", message: e.message });
-    }
-  };
-
-  const handleImageUpload = async () => {
-    try {
-      const image = await chooseImageHandler();
-      if (image) {
-        let uri = image.assets[0].uri;
-        let extension = image.assets[0].uri.split(".").pop();
-        const fileName = Date.now() + "." + extension;
-
-        //create new photo file and upload
-        let picture = await fetch(uri);
-        picture = await picture.blob();
-        await uploadphoto(picture, fileName);
-        if (
-          site.divesiteprofilephoto !== null ||
-          site.divesiteprofilephoto === ""
-        ) {
-          await removePhoto({
-            filePath: `https://pub-c089cae46f7047e498ea7f80125058d5.r2.dev/`,
-            fileName: site.divesiteprofilephoto.split("/").pop(),
-          });
-        }
-
-        setSite({
-          ...site,
-          photo: `animalphotos/public/${fileName}`,
-        });
-        const success = await updateDiveSite({
-          id: site.id,
-          bio: site.divesitebio,
-          photo: `animalphotos/public/${fileName}`,
-        });
-      }
-    } catch (e) {
-      console.log("error: Photo Selection Cancelled", e.message);
-    }
-  };
-
-  const handleEmailDS = () => {
-    const to = ["scubaseasons@gmail.com"];
-    email(to, {
-      // Optional additional arguments
-      subject: `Reporting issue with Dive Site: "${site.name}" at Latitude: ${site.lat} Longitude: ${site.lng} `,
-      body:
-        "Type of issue: \n \n 1) Dive Site name not correct \n (Please provide the correct dive site name and we will correct the record)\n \n 2)Dive Site GPS Coordinates are not correct \n (Please provide a correct latitude and longitude and we will update the record)",
-      checkCanOpen: false, // Call Linking.canOpenURL prior to Linking.openURL
-    }).catch(console.error);
-  };
-
-  const onClose = () => {
-    setLevelOneScreen(false);
-  };
-
-  const openPicUploader = () => {
-    setPinValues({
-      ...pinValues,
-      Latitude: String(selectedDiveSite.Latitude),
-      Longitude: String(selectedDiveSite.Longitude),
-      siteName: selectedDiveSite.SiteName,
+    // setLevelTwoScreen(false);
+    setSite({
+      ...site,
+      photo: null,
     });
-    setLevelOneScreen(false);
-    setPreviousButtonID(activeScreen);
-    setActiveScreen("PictureUploadScreen");
-    useButtonPressHelper(
-      "PictureUploadScreen",
-      activeScreen,
-      levelTwoScreen,
-      setLevelTwoScreen
-    );
   };
+
+  window.addEventListener("resize", trackDimensions);
+
+  const [windowWidth, setWindowWidth] = useState(screenWidthInital);
+  const [windowHeight, setWindowHeigth] = useState(screenHeitghInital);
+
+  function trackDimensions() {
+    setWindowWidth(window.innerWidth);
+    setWindowHeigth(window.innerHeight);
+  }
 
   return (
-    <div style={styles.container}>
-      <MaterialIcons
-        name="chevron-left"
-        size={moderateScale(48)}
-        color={colors.themeWhite}
-        onPress={() => onClose()}
-        style={styles.backButton}
-      />
-        <div style={styles.contributeButton} onClick={openPicUploader}>
-  <p style={styles.contributeButtonText}>{screenData.DiveSite.addSightingButton}</p>
-        </div>
-
-      {profile[0].partnerAccount && (
-        <div style={styles.addPhotoButton}>
+    <div
+      className="$themeWhite"
+      style={{
+        display: 'flex',
+        width: "100%",
+        height: "100%",
+        backgroundColor: "$themeWhite",
+        marginBottom: "100%",
+      }}
+    >
+      <div
+        style={{
+          width: "50%",
+          height: "100%",
+        }}
+      >
+        <div className={style.backButton} style={{ position: "absolute" }}>
           <MaterialIcons
-            name="add-a-photo"
-            size={moderateScale(30)}
-            color={colors.themeWhite}
-            onPress={() => handleImageUpload()}
+            name="chevron-left"
+            size={30}
+            color={"$themeWhite"}
+            onClick={() => onClose()}
           />
         </div>
-      )}
 
-      <div style={styles.contentContainer}>
-        <div style={styles.siteNameContainer}>
-          <p style={styles.header}>{site.name}</p>
-
-          <FontAwesome
-            name="flag"
-            color="maroon"
-            size={moderateScale(16)}
-            style={{ marginTop: "5%", marginLeft: moderateScale(10) }}
-            onPress={() => handleEmailDS()}
-          />
-        </div>
-        <p style={styles.contributor}>Added by: {site.newusername}</p>
-        <MaskedView
-          maskElement={
-            <LinearGradient
-              style={{ flex: 1 }}
-              colors={["white", "transparent"]}
-              start={{ x: 0.5, y: 0.7 }}
-            ></LinearGradient>
-          }
+        <div className={style.picZoneHalf} 
+        // style={{backgroundImage: `url(${defaultImage})`}}
         >
-          <div style={styles.scrolldivBox}>
-            <ScrollView>
-              {site && (
-                <PlainTextInput
-                  placeHolder={`A little about ${site.name}`}
-                  content={site.divesitebio}
-                  fontSz={fontSizes.StandardText}
-                  isPartnerAccount={isPartnerAccount}
-                  isEditModeOn={isEditModeOn}
-                  setIsEditModeOn={setIsEditModeOn}
-                  onChangeText={(bioText) =>
-                    setSite({ ...site, divesitebio: bioText })
-                  }
+          {/* {picUrl ? 
+            <img src={picUrl} width={"100%"} className={style.picStylesHalf}></img>
+            :
+            <img src={defaultImage} width={"100%"} className={style.picStylesHalf}></img>
+          } */}
+          
+
+          <FormGroup>
+            <Input
+              placeholder="Upload"
+              className="modalInputs2"
+              style={{
+                textAlign: "center",
+                display: "none",
+              }}
+              id="file"
+              type="file"
+              name="picFile"
+              bsSize="lg"
+              onChange={handleImageSelection}
+            ></Input>
+          </FormGroup>
+
+          {picUrl ? (
+            <div
+              style={{ position: "absolute", right: "5%", marginTop: "40%" }}
+            >
+              <MaterialIcons
+                name="add-a-photo"
+                size={30}
+                color={"white"}
+                onClick={() => handleClick()}
+              />
+            </div>
+          ) : null}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            marginLeft: "5%",
+            marginRight: "5%",
+            height: "50%",
+            zIndex: 22,
+            // marginTop: '-10%'
+          }}
+        >
+          <div
+            style={{
+              height: 400,
+              width: "100%",
+              marginLeft: "5%",
+              marginBottom: "5%",
+              overflowY: "auto",
+              // zIndex: 2
+            }}
+          >
+            <p className={style.headerText}>{screenData.PicUploader.header}</p>
+
+            <div
+              style={{
+                marginBottom: "20%",
+                width: "75%",
+                alignItems: "center",
+                // zIndex: 2
+              }}
+            >
+              <div style={null}>
+                <p className={style.inputLabels}>
+                  {screenData.PicUploader.whenLabel}
+                </p>
+                <div pointerEvents="none">
+                  <TextInputField
+                    dataType="date"
+                    icon={"calendar-month-outline"}
+                    inputValue={null}
+                    placeHolderText={screenData.PicUploader.whenPlaceholder}
+                    secure={false}
+                    vectorIcon={"MaterialCommunityIcons"}
+                  />
+                </div>
+              </div>
+              <div style={null}>
+                <p className={style.inputLabels}>
+                  {screenData.PicUploader.whereLabel}
+                </p>
+                <TextInputField
+                  dataType="text"
+                  icon={"anchor"}
+                  inputValue={null}
+                  placeHolderText={screenData.PicUploader.wherePlaceholder}
+                  secure={false}
                 />
-              )}
-            </ScrollView>
+              </div>
+            </div>
           </div>
-        </MaskedView>
+        </div>
+
+        <WavyHeader
+          customStyles={'50%'}
+          image={site.photo}
+          setPin={setSite}
+          pin={site}
+        ></WavyHeader>
       </div>
+      <div
+        style={{
+          width: "50%",
+          height: "100%",
+        }}
+      >
 
-      <WavyHeaderDynamic
-        customStyles={styles.svgCurve}
-        image={site && site.divesiteprofilephoto}
-      ></WavyHeaderDynamic>
-
-      <BottomDrawer
-        dataSet={diveSitePics}
-        dataSetType={"DiveSitePhotos"}
-        lowerBound={drawerLowerBound}
-        upperBound={drawerUpperBound}
-        drawerHeader={screenData.DiveSite.drawerHeader}
-        emptyDrawer={screenData.DiveSite.emptyDrawer}
-      />
+        <p>HI</p>
+        </div>
     </div>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    zIndex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
-    height: windowHeight,
-  },
-  contentContainer: {
-    alignItems: "left",
-    zIndex: 1,
-    position: "absolute",
-    top: 0,
-    left: 0,
-    marginTop: Platform.OS === "ios" ? windowHeight / 2.4 : windowHeight / 2.2,
-    width: "100%",
-    height: 300,
-    // backgroundColor: "pink",
-  },
-  siteNameContainer: {
-    // zIndex: 1,
-    flexDirection: "row",
-    width: "auto",
-    marginTop: Platform.OS === "ios" ? windowHeight / 50 : windowHeight / 50,
-    marginLeft: "8%",
-  },
-  header: {
-    // zIndex: 50,
-    marginTop: "5%",
-    fontSize: moderateScale(fontSizes.Header),
-    fontFamily: activeFonts.Regular,
-    color: colors.themeBlack,
-  },
-  contributor: {
-    // zIndex: 50,
-    fontSize: moderateScale(fontSizes.SmallText),
-    fontFamily: activeFonts.Thin,
-    color: colors.themeBlack,
-    marginLeft: "12%",
-  },
-  scrollViewBox: {
-    // zIndex: 5,
-    marginTop: "3%",
-    marginLeft: "2%",
-    height: windowHeight / 6,
-    // backgroundColor: "green"
-  },
-  screenCloseButton: [
-    { zIndex: 1, position: "absolute", top: "6%", right: "5%" },
-  ],
-  contributeButton: [
-    { zIndex: 10, position: "absolute", top: "6%", right: "3%" },
-    screenSecondaryButton,
-  ],
-  backButton: [{ zIndex: 10, position: "absolute", top: "5.5%", left: "2%" }],
-  contributeButtonText: [buttonTextAlt, { marginHorizontal: moderateScale(5) }],
-  addPhotoButton: [
-    { zIndex: 10, position: "absolute", top: "32%", right: "5%" },
-  ],
-  svgCurve: {
-    position: "absolute",
-    bottom: 0,
-    width: Dimensions.get("window").width,
-  },
-  page: {
-    position: "absolute",
-    zIndex: 1,
-    // bottom: -200,
-    width: "100%",
-    // marginLeft: "1%",
-    // marginTop: -windowHeight,
-    height: "100%",
-  },
-  pageContainer: {
-    // pointerEvents: "none",
-    // justifyContent: "center",
-    position: "absolute",
-    // top: 0,
-    // left: "5%",
-    // backgroundColor: "yellow",
-  },
-  gapBox: { pointerEvents: "none", height: windowHeight * 0.75 },
-  shadowbox: {
-    flex: 1,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 2,
-      height: 2,
-    },
-    shadowOpacity: 0.8,
-    shadowRadius: 5,
-
-    elevation: 10,
-  },
-});
--- wetmap/src/components/newModals/picUploader.jsx
+++ wetmap/src/components/newModals/picUploader.jsx
@@ -3,30 +3,21 @@ import screenData from "./screenData.json";
 import style from "./modalContent.module.scss";
 import LargeButton from "./largeButton";
 import Button from "./button";
-import { Container, Form, FormGroup, Label, Input } from "reactstrap";
-// import moment from "moment";
-import WavyHeaderUploader from "./wavyHeaderUploader";
+import { FormGroup, Input } from "reactstrap";
+import WavyHeader from "./wavyHeader";
 import TextInputField from "../newModals/textInput";
 import AutoSuggest from "../autoSuggest/autoSuggest";
 import { PinContext } from "../contexts/staticPinContext";
 import { UserProfileContext } from "../contexts/userProfileContext";
 import { MaterialIcons } from "react-web-vector-icons";
 import { insertPhotoWaits } from "../../supabaseCalls/photoWaitSupabaseCalls";
-import {
-  uploadphoto,
-  removePhoto,
-} from "../../cloudflareBucketCalls/cloudflareAWSCalls";
 import { handleImageUpload, clearPreviousImage } from "./imageUploadHelpers";
-import ConfirmationModal from '../modals/confirmationModal';
-import { animated, useSpring } from 'react-spring';
-import { ModalContext } from '../contexts/modalContext.jsx';
-// import { ActiveConfirmationIDContext } from "../contexts/activeConfirmationIDContext";
-// import { ConfirmationTypeContext } from "../contexts/confirmationTypeContext";
-// import { ConfirmationModalContext } from "../contexts/confirmationModalContext";
+import ConfirmationModal from "../modals/confirmationModal";
+import { animated, useSpring } from "react-spring";
+import { ModalContext } from "../contexts/modalContext.jsx";
 
 const screenWidthInital = window.innerWidth;
 const screenHeitghInital = window.innerHeight;
-
 
 export default function PicUploader(props) {
   const {} = props;
@@ -42,19 +33,18 @@ export default function PicUploader(props) {
 
   const sucessModalSlide = useSpring({
     from: { transform: `translate3d(0,0,0)` },
-    to:   { transform: `translate3d(0,${successModalYCoord}px,0)` },
+    to: { transform: `translate3d(0,${successModalYCoord}px,0)` },
   });
 
   const cautionModalSlide = useSpring({
     from: { transform: `translate3d(0,0,0)` },
-    to:   { transform: `translate3d(0,${cautionModalYCoord}px,0)` },
+    to: { transform: `translate3d(0,${cautionModalYCoord}px,0)` },
   });
 
   const animateSuccessModal = () => {
     if (successModalYCoord === 0) {
       setSuccessModalYCoord(-windowHeight);
-    }
-    else {
+    } else {
       setSuccessModalYCoord(0);
     }
   };
@@ -62,13 +52,10 @@ export default function PicUploader(props) {
   const animateCautionModal = () => {
     if (cautionModalYCoord === 0) {
       setCautionModalYCoord(-windowHeight);
-    }
-    else {
+    } else {
       setCautionModalYCoord(0);
     }
   };
-
-
 
   function handleClick() {
     document.getElementById("file").click();
@@ -95,7 +82,7 @@ export default function PicUploader(props) {
         import.meta.env.VITE_CLOUDFLARE_R2_BUCKET_PATH + `${photoName}`
       );
     } else {
-      setPicUrl(null)
+      setPicUrl(null);
     }
   }, [pin.PicFile]);
 
@@ -106,9 +93,9 @@ export default function PicUploader(props) {
         ...pin,
         PicFile: null,
         Animal: "",
-        PicDate: ""
+        PicDate: "",
       });
-      setPicUrl(null)
+      setPicUrl(null);
       animateSuccessModal();
     } else {
       animateCautionModal();
@@ -131,8 +118,7 @@ export default function PicUploader(props) {
     });
   };
 
-
-  window.addEventListener('resize', trackDimensions);
+  window.addEventListener("resize", trackDimensions);
 
   const [windowWidth, setWindowWidth] = useState(screenWidthInital);
   const [windowHeight, setWindowHeigth] = useState(screenHeitghInital);
@@ -163,11 +149,7 @@ export default function PicUploader(props) {
 
       <div className={style.picZone}>
         {picUrl ? (
-          <img
-            src={picUrl}
-            width={"100%"}
-            className={style.picStyles}
-          ></img>
+          <img src={picUrl} width={"100%"} className={style.picStyles}></img>
         ) : (
           <div style={{ paddingTop: "25%" }}>
             <LargeButton
@@ -195,7 +177,7 @@ export default function PicUploader(props) {
         </FormGroup>
 
         {picUrl ? (
-          <div style={{ position: 'absolute', right: '5%',  marginTop: "40%" }}>
+          <div style={{ position: "absolute", right: "5%", marginTop: "40%" }}>
             <MaterialIcons
               name="add-a-photo"
               size={30}
@@ -216,54 +198,56 @@ export default function PicUploader(props) {
           height: "50%",
         }}
       >
-        <p className={style.headerText}>{screenData.PicUploader.header}</p>
+        <div style={{ height:400, width: '100%',  marginLeft: "5%", marginBottom: "5%", overflowY: 'auto'}}>
+          <p className={style.headerText}>{screenData.PicUploader.header}</p>
 
-        <div
-          style={{
-            marginBottom: "20%",
-            width: "75%",
-            alignItems: "center",
-          }}
-        >
-          <div style={null}>
-            <p className={style.inputLabels}>
-              {screenData.PicUploader.whatLabel}
-            </p>
-            <AutoSuggest
-              pin={pin}
-              setPin={setPin}
-              inputValue={pin.Animal}
-              icon={"shark"}
-              placeHolderText={screenData.PicUploader.whatPlaceholder}
-              vectorIcon={"MaterialCommunityIcons"}
-            />
-          </div>
-          <div style={null}>
-            <p className={style.inputLabels}>
-              {screenData.PicUploader.whenLabel}
-            </p>
-            <div pointerEvents="none">
-              <TextInputField
-                dataType="date"
-                icon={"calendar-month-outline"}
-                inputValue={pin.PicDate}
-                placeHolderText={screenData.PicUploader.whenPlaceholder}
-                secure={false}
+          <div
+            style={{
+              marginBottom: "20%",
+              width: "75%",
+              alignItems: "center",
+            }}
+          >
+            <div style={null}>
+              <p className={style.inputLabels}>
+                {screenData.PicUploader.whatLabel}
+              </p>
+              <AutoSuggest
+                pin={pin}
+                setPin={setPin}
+                inputValue={pin.Animal}
+                icon={"shark"}
+                placeHolderText={screenData.PicUploader.whatPlaceholder}
                 vectorIcon={"MaterialCommunityIcons"}
               />
             </div>
-          </div>
-          <div style={null}>
-            <p className={style.inputLabels}>
-              {screenData.PicUploader.whereLabel}
-            </p>
-            <TextInputField
-              dataType="text"
-              icon={"anchor"}
-              inputValue={pin.siteName}
-              placeHolderText={screenData.PicUploader.wherePlaceholder}
-              secure={false}
-            />
+            <div style={null}>
+              <p className={style.inputLabels}>
+                {screenData.PicUploader.whenLabel}
+              </p>
+              <div pointerEvents="none">
+                <TextInputField
+                  dataType="date"
+                  icon={"calendar-month-outline"}
+                  inputValue={pin.PicDate}
+                  placeHolderText={screenData.PicUploader.whenPlaceholder}
+                  secure={false}
+                  vectorIcon={"MaterialCommunityIcons"}
+                />
+              </div>
+            </div>
+            <div style={null}>
+              <p className={style.inputLabels}>
+                {screenData.PicUploader.whereLabel}
+              </p>
+              <TextInputField
+                dataType="text"
+                icon={"anchor"}
+                inputValue={pin.siteName}
+                placeHolderText={screenData.PicUploader.wherePlaceholder}
+                secure={false}
+              />
+            </div>
           </div>
         </div>
 
@@ -276,14 +260,14 @@ export default function PicUploader(props) {
         </div>
       </div>
 
-      <WavyHeaderUploader
-        customStyles={{ position: "absolute", bottom: 500, width: "100%" }}
+      <WavyHeader
+        customStyles={"100%"}
         image={pin.PicFile}
         setPin={setPin}
         pin={pin}
-      ></WavyHeaderUploader>
+      ></WavyHeader>
 
-<animated.div
+      <animated.div
         className="successModal modalBase"
         style={sucessModalSlide}
         ref={successModalRef}
