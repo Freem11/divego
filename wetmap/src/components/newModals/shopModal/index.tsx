640783fc0b46b07cb7a89964a747722b46b52afd
Andrew Howe <andrewhowe1000@gmail.com>
2024-11-05 10:47:18 -0700

    move component to view.tsx, logic to be separated into index.tsx

--- wetmap/src/components/newModals/shopModal/index.tsx
+++ wetmap/src/components/newModals/shopModal/index.tsx
@@ -9,89 +9,52 @@ import { ZoomHelperContext } from '../../contexts/zoomHelperContext';
 import './shopModal.css';
 import { shops } from '../../../supabaseCalls/shopsSupabaseCalls';
 import CloseButton from '../../closeButton/closeButton';
+import shopModalView from './view';
+import ShopModalView from './view';
 
 export default function ShopModal(props) {
   // const {lat, lng, setSelectedPhoto, setPhotoBoxModel } = props
-  const { shopModal, setShopModal } = useContext(ShopModalContext);
-  const { selectedShop, setSelectedShop } = useContext(SelectedShopContext);
-  const [siteCloseState, setSiteCloseState] = useState(false);
-  const [itineraryList, setItineraryList] = useState('');
-  const [selectedID, setSelectedID] = useState(null);
-  const { masterSwitch, setMasterSwitch } = useContext(MasterContext);
-  const { mapCoords, setMapCoords } = useContext(CoordsContext);
-  const { zoomHelper, setZoomHelper } = useContext(ZoomHelperContext);
+  // const { shopModal, setShopModal } = useContext(ShopModalContext);
+  // const { selectedShop, setSelectedShop } = useContext(SelectedShopContext);
+  // const [siteCloseState, setSiteCloseState] = useState(false);
+  // const [itineraryList, setItineraryList] = useState('');
+  // const [selectedID, setSelectedID] = useState(null);
+  // const { masterSwitch, setMasterSwitch } = useContext(MasterContext);
+  // const { mapCoords, setMapCoords } = useContext(CoordsContext);
+  // const { zoomHelper, setZoomHelper } = useContext(ZoomHelperContext);
 
-  useEffect(() => {
-    if (selectedShop[0]) {
-      getItineraries(selectedShop[0].id);
-      setMasterSwitch(true);
-    }
-  }, [selectedShop]);
+  // useEffect(() => {
+  //   if (selectedShop[0]) {
+  //     getItineraries(selectedShop[0].id);
+  //     setMasterSwitch(true);
+  //   }
+  // }, [selectedShop]);
 
-  useEffect(() => {
-    if (shopModal && zoomHelper) {
-      setMapCoords([selectedShop[0].lat, selectedShop[0].lng]);
-    }
-  }, [shopModal]);
+  // useEffect(() => {
+  //   if (shopModal && zoomHelper) {
+  //     setMapCoords([selectedShop[0].lat, selectedShop[0].lng]);
+  //   }
+  // }, [shopModal]);
 
-  const getItineraries = async (IdNum) => {
-    try {
-      const itins = await itineraries(IdNum);
-      if (itins.length > 0) {
-        setItineraryList(itins);
-      }
-    }
-    catch (e) {
-      console.log({ title: 'Error', message: e.message });
-    }
-  };
+  // const getItineraries = async (IdNum) => {
+  //   try {
+  //     const itins = await itineraries(IdNum);
+  //     if (itins.length > 0) {
+  //       setItineraryList(itins);
+  //     }
+  //   }
+  //   catch (e) {
+  //     console.log({ title: 'Error', message: e.message });
+  //   }
+  // };
 
-  const handleShopModalClose = () => {
-    setSelectedShop({ ...selectedShop, id: 0, orgName: '' });
-    setItineraryList('');
-    setShopModal(false);
-  };
+  // const handleShopModalClose = () => {
+  //   setSelectedShop({ ...selectedShop, id: 0, orgName: '' });
+  //   setItineraryList('');
+  //   setShopModal(false);
+  // };
 
   return (
-    <div
-      style={{
-        height:   '98%',
-        // backgroundColor: "orange",
-        overflow: 'hidden',
-      }}
-    >
-      <div className="titleAlt">
-        <div>
-          <h3 className="headerAlt">
-            {selectedShop[0] && selectedShop[0].orgName}
-          </h3>
-        </div>
-        <CloseButton onClick={handleShopModalClose} />
-      </div>
-
-      <div style={{ marginTop: '3%', width: '100%', borderRadius: 15 }}>
-        <div className="container5">
-          {itineraryList
-          && itineraryList.map((itinerary) => {
-            return (
-              <Itinerary
-                key={itinerary.id}
-                itinerary={itinerary}
-                setSelectedID={setSelectedID}
-                selectedID={selectedID}
-                setShopModal={setShopModal}
-              />
-            );
-          })}
-          {itineraryList.length === 0 && (
-            <div>
-              <p className="noSightings">
-                No Trips are currently being offered.
-              </p>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
+    <ShopModalView />
   );
 }
--- wetmap/src/components/newModals/shopModal/view.tsx
+++ wetmap/src/components/newModals/shopModal/view.tsx
@@ -1,112 +1,98 @@
-import React, { useRef } from 'react';
-import screenData from '../screenData.json';
-import style from './style.module.scss';
-import Picture from '../../modals/picture';
-import Button from '../../reusables/button';
-import Icon from '../../../icons/Icon';
-import defaultHeaderPicture from '../../../images/blackManta.png';
-import { DiveSiteWithUserName } from '../../../entities/diveSite';
-import { PhotosGroupedByDate } from '../../../entities/photos';
-import PlainTextInput from '../../reusables/plainTextInput';
-import WavyModalHeader from '../../reusables/wavyModalHeader';
-import ButtonIcon from '../../reusables/buttonIcon';
+import React, { useState, useContext, useEffect } from 'react';
+import Itinerary from '../../itineraries/itinerary';
+import { itineraries } from '../../../supabaseCalls/itinerarySupabaseCalls';
+import { SelectedShopContext } from '../../contexts/selectedShopContext';
+import { ShopModalContext } from '../../contexts/shopModalContext';
+import { MasterContext } from '../../contexts/masterContext';
+import { CoordsContext } from '../../contexts/mapCoordsContext';
+import { ZoomHelperContext } from '../../contexts/zoomHelperContext';
+import './shopModal.css';
+import { shops } from '../../../supabaseCalls/shopsSupabaseCalls';
+import CloseButton from '../../closeButton/closeButton';
+import shopModalView from './view';
 
-type DiveSiteViewProps = {
-  onClose:              () => void
-  openPicUploader:      () => void
-  handleImageSelection: (event: React.ChangeEvent<HTMLInputElement>) => void
-  onDiveSiteBioChange:  (newValue: string) => void
-  diveSite:             DiveSiteWithUserName | null
-  diveSitePics:         PhotosGroupedByDate[] | null
-  isPartnerAccount:     boolean
-  headerPictureUrl:     string | null
-};
 
-export default function DiveSiteView(props: DiveSiteViewProps) {
-  const fileUploaderRef = useRef<HTMLInputElement>(null);
+export default function ShopModalView(props) {
+  // const {lat, lng, setSelectedPhoto, setPhotoBoxModel } = props
+  const { shopModal, setShopModal } = useContext(ShopModalContext);
+  const { selectedShop, setSelectedShop } = useContext(SelectedShopContext);
+  const [siteCloseState, setSiteCloseState] = useState(false);
+  const [itineraryList, setItineraryList] = useState('');
+  const [selectedID, setSelectedID] = useState(null);
+  const { masterSwitch, setMasterSwitch } = useContext(MasterContext);
+  const { mapCoords, setMapCoords } = useContext(CoordsContext);
+  const { zoomHelper, setZoomHelper } = useContext(ZoomHelperContext);
+
+  useEffect(() => {
+    if (selectedShop[0]) {
+      getItineraries(selectedShop[0].id);
+      setMasterSwitch(true);
+    }
+  }, [selectedShop]);
+
+  useEffect(() => {
+    if (shopModal && zoomHelper) {
+      setMapCoords([selectedShop[0].lat, selectedShop[0].lng]);
+    }
+  }, [shopModal]);
+
+  const getItineraries = async (IdNum) => {
+    try {
+      const itins = await itineraries(IdNum);
+      if (itins.length > 0) {
+        setItineraryList(itins);
+      }
+    }
+    catch (e) {
+      console.log({ title: 'Error', message: e.message });
+    }
+  };
+
+  const handleShopModalClose = () => {
+    setSelectedShop({ ...selectedShop, id: 0, orgName: '' });
+    setItineraryList('');
+    setShopModal(false);
+  };
+
   return (
-    <div className="cols mx-0 full-height">
-      <input
-        ref={fileUploaderRef}
-        className="d-hide"
-        type="file"
-        onChange={props.handleImageSelection}
-      />
-
-      <div className="col-6">
-
-        <WavyModalHeader image={props.headerPictureUrl || defaultHeaderPicture} onClose={props.onClose}>
-          <div className={style.buttonOpenPictureUpload}>
-            <Button
-              className="btn-lg"
-              onClick={props.openPicUploader}
-            >
-              <span className="hide-sm">{screenData.DiveSite.addSightingButton}</span>
-            </Button>
-          </div>
-
-          <div className={style.buttonImageUpload}>
-            <ButtonIcon
-              icon={<Icon name="camera-plus" />}
-              className="btn-lg"
-              onClick={() => fileUploaderRef?.current?.click?.()}
-            />
-          </div>
-        </WavyModalHeader>
-
-        <div className="ml-6">
-          <div className="stack-4">
-            <div>
-              <div className="d-flex">
-                <h1 className="mb-0">{props?.diveSite?.name}</h1>
-                <div>
-                  <Icon
-                    name="flag"
-                    fill="maroon"
-                    width="30px"
-                    onClick={() => window.location = `mailto:DiveGo2022@gmail.com?subject=Reporting%20issue%20with%20Dive%20Site:%20"${selectedDiveSite.SiteName}"%20at%20Latitude:%20${selectedDiveSite.Latitude}%20Longitude:%20${selectedDiveSite.Longitude}&body=Type%20of%20issue:%0D%0A%0D%0A%0D%0A%0D%0A1)%20Dive%20site%20name%20not%20correct%0D%0A%0D%0A(Please%20provide%20correct%20dive%20site%20name%20and%20we%20will%20correct%20the%20record)%0D%0A%0D%0A%0D%0A%0D%0A2)%20Dive%20site%20GPS%20coordinates%20are%20not%20correct%0D%0A%0D%0A(Please%20provide%20a%20correct%20latitude%20and%20longitude%20and%20we%20will%20update%20the%20record)`}
-                  />
-                </div>
-              </div>
-
-              <div className="d-flex">
-                {'Added by: '}
-                <a href="#">{props?.diveSite?.newusername}</a>
-              </div>
-            </div>
-
-            <div className="panel border-none">
-              <div className="panel-body">
-                <PlainTextInput
-                  placeHolder={`A little about ${props?.diveSite?.name}`}
-                  content={props?.diveSite?.divesitebio || ''}
-                  readOnly={!props?.isPartnerAccount}
-                  onSubmit={props?.onDiveSiteBioChange}
-                />
-              </div>
-            </div>
-          </div>
+    <div
+      style={{
+        height:   '98%',
+        // backgroundColor: "orange",
+        overflow: 'hidden',
+      }}
+    >
+      <div className="titleAlt">
+        <div>
+          <h3 className="headerAlt">
+            {selectedShop[0] && selectedShop[0].orgName}
+          </h3>
         </div>
+        <CloseButton onClick={handleShopModalClose} />
       </div>
 
-      <div className="col-6 panel border-none full-height">
-        <div className="panel-header">
-          <h3>{screenData.DiveSite.drawerHeader}</h3>
-        </div>
-        <div className="panel-body">
-          {props?.diveSitePics?.map((packet) => {
+      <div style={{ marginTop: '3%', width: '100%', borderRadius: 15 }}>
+        <div className="container5">
+          {itineraryList
+          && itineraryList.map((itinerary) => {
             return (
-              <div key={packet.dateTaken}>
-                <div className="">{packet.dateTaken}</div>
-                {packet.photos
-                && packet.photos.map((pic) => {
-                  return <Picture key={pic.id} pic={pic}></Picture>;
-                })}
-              </div>
+              <Itinerary
+                key={itinerary.id}
+                itinerary={itinerary}
+                setSelectedID={setSelectedID}
+                selectedID={selectedID}
+                setShopModal={setShopModal}
+              />
             );
           })}
+          {itineraryList.length === 0 && (
+            <div>
+              <p className="noSightings">
+                No Trips are currently being offered.
+              </p>
+            </div>
+          )}
         </div>
-        <div className="panel-footer"></div>
       </div>
     </div>
   );
