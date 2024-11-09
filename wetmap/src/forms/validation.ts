54286a9afa180a6a58a0f3a56cd7dade57e1f66c
taras <tymchenko.dev@gmail.com>
2024-11-08 19:18:35 -0700

    site submitter view prepare

---
+++ wetmap/src/forms/validation.ts
@@ -0,0 +1,14 @@
+
+export const _vTest = {
+  max: {
+    value:   10,
+    message: 'This value must be less than 10',
+  },
+};
+
+export const _vEmail = {
+  pattern: {
+    value:   /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    message: 'Please enter a valid email address',
+  },
+};
