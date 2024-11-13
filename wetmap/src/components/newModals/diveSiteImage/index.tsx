1c08b1b9645371129a6d01918659346b2d6a9f74
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2024-11-13 01:01:07 -0700

    added commented logic to index.tsx for modal

--- wetmap/src/components/newModals/diveSiteImage/index.tsx
+++ wetmap/src/components/newModals/diveSiteImage/index.tsx
@@ -1,23 +1,79 @@
 import React, { useState, useContext } from 'react';
-import { UserProfileContext } from '../contexts/userProfileContext';
-import { SelectedPictureContext } from '../contexts/selectedPictureContext';
+import { UserProfileContext } from '../../contexts/userProfileContext';
+import { SelectedPictureContext } from '../../contexts/selectedPictureContext';
 import {
   insertPhotoLike,
   deletePhotoLike,
-} from '../../supabaseCalls/photoLikeSupabaseCalls';
-import { grabProfileByUserName } from '../../supabaseCalls/accountSupabaseCalls';
-import FlagIcon from '@mui/icons-material/Flag';
-import notLiked from '../../images/Hand-Hollow-Blue.png';
-import liked from '../../images/Hand-Filled-Blue.png';
-import style from './picture.module.scss';
-import UserProfileModal from './userProfileModal';
-import { ModalContext } from '../contexts/modalContext';
-import CommentsModal from './commentsModal';
-import FullScreenModal from './fullScreenModal';
-import { ModalWindowSize } from '../reusables/modal/constants';
+} from '../../../supabaseCalls/photoLikeSupabaseCalls';
+import { grabProfileByUserName } from '../../../supabaseCalls/accountSupabaseCalls';
+// import FlagIcon from '@mui/icons-material/Flag';
+// import notLiked from '../../images/Hand-Hollow-Blue.png';
+// import liked from '../../images/Hand-Filled-Blue.png';
+// import style from './picture.module.scss';
+import UserProfileModal from '../../modals/userProfileModal';
+import { ModalContext } from '../../contexts/modalContext';
+import CommentsModal from '../../modals/commentsModal';
+import FullScreenModal from '../../modals/fullScreenModal';
+import { ModalWindowSize } from '../../reusables/modal/constants';
 import DiveSiteImageView from './view';
 
 export default function DiveSiteImage(props) {
+//   const { pic } = props;
+  const { profile } = useContext(UserProfileContext);
+//   const [picLiked, setPicLiked] = useState(pic.likedbyuser);
+//   const [likeData, setLikeData] = useState(pic.likeid);
+//   const [countOfLikes, setCountOfLikes] = useState(pic.likecount);
+  const { setSelectedPicture } = useContext(SelectedPictureContext);
+
+  const { modalShow } = useContext(ModalContext);
+
+//   let photoName = pic.photoFile.split('/').pop();
+
+//   const handleFollow = async (e, userName) => {
+//     e.stopPropagation();
+
+//     let picOwnerAccount = await grabProfileByUserName(userName);
+
+//     if (profile[0].UserID === picOwnerAccount[0].UserID) {
+//       return;
+//     }
+
+//     modalShow(UserProfileModal, {
+//       keepPreviousModal: true,
+//       selectedProfile:   picOwnerAccount[0].UserID,
+//     });
+//   };
+
+//   const handleCommentModal = () => {
+//     modalShow(CommentsModal, {
+//       keepPreviousModal: true,
+//     });
+//     setSelectedPicture(pic);
+//   };
+
+//   const handleLike = async (e) => {
+//     e.stopPropagation();
+
+//     if (picLiked) {
+//       deletePhotoLike(likeData);
+//       setPicLiked(false);
+//       setCountOfLikes(countOfLikes - 1);
+//     } else {
+//       const newRecord = await insertPhotoLike(profile[0].UserID, pic.id);
+//       setPicLiked(true);
+//       setLikeData(newRecord[0].id);
+//       setCountOfLikes(countOfLikes + 1);
+//     }
+//   };
+
+//   const handleModalOpen = () => {
+//     modalShow(FullScreenModal, {
+//       keepPreviousModal: true,
+//       size:              ModalWindowSize.FULL,
+//       src:               `https://pub-c089cae46f7047e498ea7f80125058d5.r2.dev/${photoName}`,
+//     });
+//   };
+
   return (
     <DiveSiteImageView></DiveSiteImageView>
   );
