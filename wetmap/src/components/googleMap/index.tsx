f5c13b172bbbaee8c0b3e7e0d6252b370c2780aa
Freem11 <freem1985@gmail.com>
2024-11-19 22:16:44 -0700

    web28-googleMaps: begun the process of moving logic to index

--- wetmap/src/components/googleMap/index.tsx
+++ wetmap/src/components/googleMap/index.tsx
@@ -1,13 +1,116 @@
-import React from 'react';
+import React, { useContext, useEffect, useLayoutEffect, useMemo, useState } from 'react';
 import {
   useJsApiLoader,
   Libraries,
 } from '@react-google-maps/api';
 import MapView from './view';
+import { getDiveSiteData, getHeatPointData, getShopData } from './mapDataHelpers';
+import { MapBoundsContext } from '../contexts/mapBoundariesContext';
+import { CoordsContext } from '../contexts/mapCoordsContext';
+import { ZoomContext } from '../contexts/mapZoomContext';
+import { AnimalContext } from '../contexts/animalContext';
+import { HeatPointsContext } from '../contexts/heatPointsContext';
+import { DiveSitesContext } from '../contexts/diveSitesContext';
+import { DiveSiteWithUserName } from '../../entities/diveSite';
+import { DiveShop } from '../../entities/diveShop';
 
 const libraries: Libraries = ['places', 'visualization'];
 
 export default function MapLoader() {
+  const [mapRef, setMapRef] = useState<google.maps.Map | null>(null);
+  const [newSites, setnewSites] = useState<DiveSiteWithUserName[]>([]);
+  const [newShops, setnewShops] = useState<DiveShop[]>([]);
+  const { heatpts, setHeatPts } = useContext(HeatPointsContext);
+
+  const { boundaries, setBoundaries } = useContext(MapBoundsContext);
+  const { mapCoords, setMapCoords } = useContext(CoordsContext);
+  const { mapZoom, setMapZoom } = useContext(ZoomContext);
+
+  const { animalVal } = useContext(AnimalContext);
+  const { divesTog } = useContext(DiveSitesContext);
+  const handleOnLoad = (map: google.maps.Map) => {
+    setMapRef(map);
+  };
+
+  let mapCenterTimoutHandler: number | undefined;
+  let mapBoundariesTimoutHandler: number;
+
+  const center = useMemo(() => ({ lat: mapCoords[0], lng: mapCoords[1] }), []);
+  const zoom = useMemo(() => mapZoom, []);
+
+
+  const handleMapUpdates = async () => {
+    if (mapRef) {
+      const boundaries: google.maps.LatLngBounds | undefined = mapRef.getBounds();
+      if (boundaries) {
+        const latHi = boundaries.getNorthEast().lat();
+        const latLo = boundaries.getSouthWest().lat();
+        const lngE = boundaries.getNorthEast().lng();
+        const lngW = boundaries.getSouthWest().lng();
+
+        const diveSiteList = await getDiveSiteData(latHi, latLo, lngE, lngW);
+        setnewSites(!divesTog ? [] : diveSiteList);
+
+        const heatPointList = await getHeatPointData(latHi, latLo, lngE, lngW, animalVal);
+        setHeatPts(heatPointList);
+
+        const shoplist = await getShopData(latHi, latLo, lngE, lngW);
+        setnewShops(!divesTog ? [] : shoplist);
+      }
+    }
+  };
+
+  const handleBoundsChange = async () => {
+    if (mapRef) {
+      window.clearTimeout(mapBoundariesTimoutHandler);
+      mapBoundariesTimoutHandler = window.setTimeout(function () {
+        const boundaries: google.maps.LatLngBounds | undefined = mapRef?.getBounds();
+        if (boundaries) {
+          const latHi = boundaries.getNorthEast().lat();
+          const latLo = boundaries.getSouthWest().lat();
+          const lngE = boundaries.getNorthEast().lng();
+          const lngW = boundaries.getSouthWest().lng();
+          setBoundaries([lngW, latLo, lngE, latHi]);
+        }
+        handleMapUpdates();
+      }, 50);
+    }
+  };
+
+  const handleMapCenterChange = async () => {
+    if (mapRef) {
+      const position = mapRef.getCenter();
+      if (position) {
+        window.clearTimeout(mapCenterTimoutHandler);
+        mapCenterTimoutHandler = window.setTimeout(function () {
+          setMapCoords([position.lat(), position.lng()]);
+          handleMapUpdates();
+        }, 50);
+      }
+    }
+  };
+
+  const handleMapZoomChange = async () => {
+    if (mapRef) {
+      setMapZoom(mapRef.getZoom());
+      handleMapUpdates();
+    }
+  };
+
+  useLayoutEffect(() => {
+    setMapCoords([center.lat, center.lng]);
+    setMapZoom(zoom);
+    handleMapUpdates();
+  }, []);
+
+  useEffect(() => {
+    if (mapRef) {
+      mapRef.setZoom(mapZoom);
+      handleMapZoomChange();
+    }
+  }, [mapZoom]);
+
+
   const { isLoaded } = useJsApiLoader({
     id:               'google-map-script',
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
@@ -15,5 +118,27 @@ export default function MapLoader() {
   });
 
   if (!isLoaded) return <div>Loading...</div>;
-  return <MapView></MapView>;
+  return (
+    <MapView
+      mapRef={mapRef}
+      zoom={zoom}
+      center={center}
+      animalVal={animalVal}
+      newSites={newSites}
+      newShops={newShops}
+      heatpts={heatpts}
+      divesTog={divesTog}
+      boundaries={boundaries}
+      mapCoords={mapCoords}
+      setMapCoords={setMapCoords}
+      mapZoom={mapZoom}
+      setMapZoom={setMapZoom}
+      onLoad={handleOnLoad}
+      handleMapUpdates={handleMapUpdates}
+      handleBoundsChange={handleBoundsChange}
+      handleMapCenterChange={handleMapCenterChange}
+      handleMapZoomChange={handleMapZoomChange}
+    >
+    </MapView>
+  );
 }
--- wetmap/src/components/googleMap/view.tsx
+++ wetmap/src/components/googleMap/view.tsx
@@ -17,30 +17,46 @@ import {
   useEffect,
   useLayoutEffect,
 } from 'react';
-import { CoordsContext } from '../contexts/mapCoordsContext';
-import { ZoomContext } from '../contexts/mapZoomContext';
-import { DiveSitesContext } from '../contexts/diveSitesContext';
-import { AnimalContext } from '../contexts/animalContext';
+
+
 import { MasterContext } from '../contexts/masterContext';
 import { MinorContext } from '../contexts/minorContext';
 import { PinSpotContext } from '../contexts/pinSpotContext';
 import { SelectedDiveSiteContext } from '../contexts/selectedDiveSiteContext';
 import { SelectedShopContext } from '../contexts/selectedShopContext';
-import { HeatPointsContext } from '../contexts/heatPointsContext';
-import { MapBoundsContext } from '../contexts/mapBoundariesContext';
+
 import { DiveSpotContext } from '../contexts/diveSpotContext';
 import { ShopModalContext } from '../contexts/shopModalContext';
 import { SitesArrayContext } from '../contexts/sitesArrayContext';
 import { ZoomHelperContext } from '../contexts/zoomHelperContext';
 import { CarrouselTilesContext } from '../contexts/carrouselTilesContext';
-import { getDiveSiteData, getHeatPointData, getShopData } from './mapDataHelpers';
 import { setupClusters, setupShopClusters, setupPinConfigs } from './mapPinHelpers';
 import { ModalContext } from '../contexts/modalContext';
 import { DiveSiteWithUserName } from '../../entities/diveSite';
 import { DiveShop } from '../../entities/diveShop';
 
-export default function MapView() {
-  const [mapRef, setMapRef] = useState<google.maps.Map | null>(null);
+type MapViewProps = {
+  mapRef:                google.maps.Map | null
+  zoom:                  number
+  center:                { lat: number, lng: number }
+  animalVal:             string[]
+  divesTog:              boolean
+  newSites:              DiveSiteWithUserName[]
+  newShops:              DiveShop[]
+  heatpts:               []
+  boundaries:            google.maps.LatLngBounds
+  mapCoords:             number[]
+  setMapCoords:          (coords: number[]) => void
+  mapZoom:               number
+  setMapZoom:            (zoomLev: number) => void
+  onLoad:                (map: google.maps.Map) => void
+  handleMapUpdates:      () => void
+  handleBoundsChange:    () => void
+  handleMapCenterChange: () => void
+  handleMapZoomChange:   () => void
+};
+
+export default function MapView(props: MapViewProps) {
   const [pinRef, setPinRef] = useState<google.maps.Marker | null>(null);
   const [tempMarker, setTempMarker] = useState<TempMarker | null>(null);
 
@@ -49,33 +65,22 @@ export default function MapView() {
   const { masterSwitch } = useContext(MasterContext);
   const { minorSwitch, setMinorSwitch } = useContext(MinorContext);
   const { addSiteVals, setAddSiteVals } = useContext(DiveSpotContext);
-  const { mapCoords, setMapCoords } = useContext(CoordsContext);
-  const { mapZoom, setMapZoom } = useContext(ZoomContext);
-  const { divesTog } = useContext(DiveSitesContext);
-  const { boundaries, setBoundaries } = useContext(MapBoundsContext);
+
+
   const { zoomHelper, setZoomHelper } = useContext(ZoomHelperContext);
-  const { animalVal } = useContext(AnimalContext);
   const { selectedDiveSite, setSelectedDiveSite } = useContext(
     SelectedDiveSiteContext,
   );
   const { selectedShop, setSelectedShop } = useContext(SelectedShopContext);
-  const { heatpts, setHeatPts } = useContext(HeatPointsContext);
 
   const { shopModal } = useContext(ShopModalContext);
 
   const { setTiles } = useContext(CarrouselTilesContext);
 
   const { sitesArray } = useContext(SitesArrayContext);
-  const [newSites, setnewSites] = useState<DiveSiteWithUserName[]>([]);
-  const [newShops, setnewShops] = useState<DiveShop[]>([]);
 
   const { modalShow } = useContext(ModalContext);
 
-  const center = useMemo(() => ({ lat: mapCoords[0], lng: mapCoords[1] }), []);
-  const zoom = useMemo(() => mapZoom, []);
-
-  let mapCenterTimoutHandler: number | undefined;
-  let mapBoundariesTimoutHandler: number;
 
   const mapConfigs = useMemo(() => ({
     mapTypeId:         'hybrid',
@@ -92,93 +97,20 @@ export default function MapView() {
     radius:  16,
   }), []);
 
-  const handleMapUpdates = async () => {
-    if (mapRef) {
-      const boundaries: google.maps.LatLngBounds | undefined = mapRef.getBounds();
-      if (boundaries) {
-        const latHi = boundaries.getNorthEast().lat();
-        const latLo = boundaries.getSouthWest().lat();
-        const lngE = boundaries.getNorthEast().lng();
-        const lngW = boundaries.getSouthWest().lng();
-
-        const diveSiteList = await getDiveSiteData(latHi, latLo, lngE, lngW);
-        setnewSites(!divesTog ? [] : diveSiteList);
-
-        const heatPointList = await getHeatPointData(latHi, latLo, lngE, lngW, animalVal);
-        setHeatPts(heatPointList);
-
-        const shoplist = await getShopData(latHi, latLo, lngE, lngW);
-        setnewShops(!divesTog ? [] : shoplist);
-      }
-    }
-  };
-
-  useLayoutEffect(() => {
-    setMapCoords([center.lat, center.lng]);
-    setMapZoom(zoom);
-    handleMapUpdates();
-  }, []);
 
   useEffect(() => {
-    setDragPin({ lat: mapCoords[0], lng: mapCoords[1] });
+    setDragPin({ lat: props.mapCoords[0], lng: props.mapCoords[1] });
   }, [masterSwitch]);
 
-  const handleOnLoad = (map: google.maps.Map) => {
-    setMapRef(map);
-  };
-
-  const handleMapCenterChange = async () => {
-    if (mapRef) {
-      const position = mapRef.getCenter();
-      if (position) {
-        window.clearTimeout(mapCenterTimoutHandler);
-        mapCenterTimoutHandler = window.setTimeout(function () {
-          setMapCoords([position.lat(), position.lng()]);
-          handleMapUpdates();
-        }, 50);
-      }
-    }
-  };
-
-  const handleMapZoomChange = async () => {
-    if (mapRef) {
-      setMapZoom(mapRef.getZoom());
-      handleMapUpdates();
-    }
-  };
 
   useEffect(() => {
-    if (mapRef) {
-      mapRef.setZoom(mapZoom);
-      handleMapZoomChange();
-    }
-  }, [mapZoom]);
-
-  const handleBoundsChange = async () => {
-    if (mapRef) {
-      window.clearTimeout(mapBoundariesTimoutHandler);
-      mapBoundariesTimoutHandler = window.setTimeout(function () {
-        const boundaries: google.maps.LatLngBounds | undefined = mapRef.getBounds();
-        if (boundaries) {
-          const latHi = boundaries.getNorthEast().lat();
-          const latLo = boundaries.getSouthWest().lat();
-          const lngE = boundaries.getNorthEast().lng();
-          const lngW = boundaries.getSouthWest().lng();
-          setBoundaries([lngW, latLo, lngE, latHi]);
-        }
-        handleMapUpdates();
-      }, 50);
-    }
-  };
-
-  useEffect(() => {
-    if (mapRef) {
-      const position = mapRef.getCenter();
+    if (props.mapRef) {
+      const position = props.mapRef.getCenter();
       if (position) {
         if (selectedDiveSite.SiteName !== '') {
           const latlng = new google.maps.LatLng(selectedDiveSite.Latitude, selectedDiveSite.Longitude);
-          mapRef.panTo(latlng);
-          setMapZoom(16);
+          props.mapRef.panTo(latlng);
+          props.setMapZoom(16);
         }
       }
       if (selectedDiveSite.Latitude !== '') {
@@ -196,13 +128,13 @@ export default function MapView() {
 
 
   useEffect(() => {
-    if (mapRef) {
-      const position = mapRef.getCenter();
+    if (props.mapRef) {
+      const position = props.mapRef.getCenter();
       if (position) {
         if (selectedShop.orgName !== '') {
           const latlng = new google.maps.LatLng(selectedShop[0].lat, selectedShop[0].lng);
-          mapRef.panTo(latlng);
-          setMapZoom(16);
+          props.mapRef.panTo(latlng);
+          props.setMapZoom(16);
         }
       }
     }
@@ -211,30 +143,30 @@ export default function MapView() {
   useEffect(() => {
     if (zoomHelper) {
       if (shopModal) {
-        setMapZoom(16);
+        props.setMapZoom(16);
         setMinorSwitch(true);
       } else if (!shopModal) {
-        setMapZoom(12);
+        props.setMapZoom(12);
         setMinorSwitch(false);
       }
       setZoomHelper(false);
     }
 
-    if (mapRef) {
-      mapRef.panTo({ lat: mapCoords[0], lng: mapCoords[1] });
+    if (props.mapRef) {
+      props.mapRef.panTo({ lat: props.mapCoords[0], lng: props.mapCoords[1] });
     }
 
-    handleMapUpdates();
-  }, [mapCoords, divesTog, animalVal]);
+    props.handleMapUpdates();
+  }, [props.mapCoords, props.divesTog, props.animalVal]);
 
-  const shopPoints: Cluster[]  = setupShopClusters(newShops);
-  const sitePoints: Cluster[]  = setupClusters(newSites, sitesArray);
+  const shopPoints: Cluster[]  = setupShopClusters(props.newShops);
+  const sitePoints: Cluster[]  = setupClusters(props.newSites, sitesArray);
   const points: Cluster[] = [...sitePoints, ...shopPoints];
 
   const { clusters, supercluster } = useSupercluster({
     points,
-    bounds:  boundaries,
-    zoom:    mapZoom,
+    bounds:  props.boundaries,
+    zoom:    props.mapZoom,
     options: { radius: 75, maxZoom: 16 },
   });
 
@@ -261,14 +193,14 @@ export default function MapView() {
 
   return (
     <GoogleMap
-      zoom={zoom}
-      center={center}
+      zoom={props.zoom}
+      center={props.center}
       mapContainerClassName={style.mapContainer}
       options={mapConfigs}
-      onLoad={handleOnLoad}
-      onCenterChanged={handleMapCenterChange}
-      onZoomChanged={handleMapZoomChange}
-      onBoundsChanged={handleBoundsChange}
+      onLoad={props.onLoad}
+      onCenterChanged={props.handleMapCenterChange}
+      onZoomChanged={props.handleMapZoomChange}
+      onBoundsChanged={props.handleBoundsChange}
       onClick={cleanupModals}
     >
 
@@ -298,12 +230,12 @@ export default function MapView() {
                   supercluster.getClusterExpansionZoom(cluster.id),
                   14,
                 );
-                if (mapRef) {
-                  const position = mapRef.getCenter();
+                if (props.mapRef) {
+                  const position = props.mapRef.getCenter();
                   if (position) {
-                    mapRef.setZoom(expansionZoom);
-                    mapRef.panTo({ lat: latitude, lng: longitude });
-                    setMapCoords([
+                    props.mapRef.setZoom(expansionZoom);
+                    props.mapRef.panTo({ lat: latitude, lng: longitude });
+                    props.setMapCoords([
                       latitude,
                       longitude,
                     ]);
@@ -326,17 +258,17 @@ export default function MapView() {
         );
       })}
 
-      {masterSwitch && heatpts.length > 0 && (
+      {masterSwitch && props.heatpts.length > 0 && (
         <HeatmapLayer
-          data={heatpts}
+          data={props.heatpts}
           options={heatpointConfigs}
         >
         </HeatmapLayer>
       )}
 
-      {!masterSwitch && !minorSwitch && heatpts.length > 0 && (
+      {!masterSwitch && !minorSwitch && props.heatpts.length > 0 && (
         <HeatmapLayer
-          data={heatpts}
+          data={props.heatpts}
           options={heatpointConfigs}
         >
         </HeatmapLayer>
