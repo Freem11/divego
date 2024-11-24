aa250565d154ee4b6eff07f4b5d8d82d5c8307c7
Favour Iheanyichukwu <90211416+fabf28@users.noreply.github.com>
2024-11-24 10:44:35 -0700

    Changes to the view for the siteSubmitter.

--- wetmap/src/components/newModals/siteSubmitter/view.tsx
+++ wetmap/src/components/newModals/siteSubmitter/view.tsx
@@ -8,11 +8,14 @@ import TextInput from '../../reusables/textInput';
 import Icon from '../../../icons/Icon';
 import Button from '../../reusables/button';
 import { Form, FormRules } from './form';
+import style from "./style.module.scss";
+
 
 
 type SiteSubmitterProps = {
   onSubmit: (data: any) => void
   values?:  Form
+  onClose: () => void
 };
 
 export default function SiteSubmitterView(props: SiteSubmitterProps) {
@@ -22,7 +25,7 @@ export default function SiteSubmitterView(props: SiteSubmitterProps) {
 
   return (
     <div className="flex-column-between full-height">
-      <WavyModalHeader image={backGroundPic} />
+      <WavyModalHeader image={backGroundPic}  onClose={props.onClose} />
 
       <form className="flex-column-between full-height mx-6 mb-6" onSubmit={handleSubmit(props.onSubmit)}>
         <div>
@@ -51,15 +54,68 @@ export default function SiteSubmitterView(props: SiteSubmitterProps) {
             />
           </div>
         </div>
+  
 
-        <Button
-          disabled={isSubmitting}
-          className="btn-lg bg-primary"
-          type="submit"
-          iconRight={<Icon name="chevron-right" />}
-        >
-          Submit
-        </Button>
+        <div className={style.horizontalButtonContainer}>
+          
+          <div className = 'col-3'>
+            <Button
+              disabled={isSubmitting}
+              className="btn-md"
+              type="submit"
+            >
+              {screenData.DiveSiteAdd.myLocationButton}
+            </Button>
+          </div>
+
+          <div className = "col-3 ">
+            <Button
+              disabled={isSubmitting}
+              className="btn-md"
+              type="submit"
+            >
+              {screenData.DiveSiteAdd.pinButton}
+            </Button>
+          </div>
+        </div>
+        {/*
+          <Button
+            onClick={getDeviceLocation}
+            btnText={screenData.DiveSiteAdd.myLocationButton}
+            helperText={screenData.DiveSiteAdd.myLocationExplainer}
+            altStyle={true}
+          />
+          <Button
+            onClick={onNavigate}
+            btnText={screenData.DiveSiteAdd.pinButton}
+            altStyle={true}
+          />
+          */}
+
+      
+        <div className={style.horizontalButtonContainer}>
+          <div className= 'col-3' ></div>
+
+          <div className='col-3'>
+            <Button
+              disabled={isSubmitting}
+              className="btn-md bg-primary"
+              type="submit"
+              iconRight={<Icon name="chevron-right" />}
+            >
+              {screenData.DiveSiteAdd.submitButton}
+            </Button>
+          </div>
+          {/*
+          <Button
+            onClick={handleSubmit}
+            btnText={screenData.DiveSiteAdd.submitButton}
+            icon={true}
+          />
+          */}
+        </div>
+        
+
       </form>
     </div>
   );
