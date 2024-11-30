e4a42ff5ce836b586b9ee13c43ee8e5d3317724f
ijromerob <ijromerob@gmail.com>
2024-11-30 16:03:30 -0700

    adding new icons and wavy image

--- wetmap/src/components/authenticationPage.jsx
+++ wetmap/src/components/authenticationPage.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -6,6 +6,13 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import SignInRoute from './signIn';
 import SignUpRoute from './signUp';
+import WavyBlock from './reusables/wavyBlock';
+import blackManta from '../images/blackManta.png';
+import Button from './reusables/button';
+import ButtonIcon from './reusables/buttonIcon';
+import googleIcon from '../images/google-color.png';
+import facebookIcon from '../images/facebook-color.png';
+import appleIcon from '../images/apple.png';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -29,47 +36,92 @@ function TabPanel(props) {
 
 TabPanel.propTypes = {
   children: PropTypes.node,
-  index:    PropTypes.number.isRequired,
-  value:    PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
   return {
-    'id':            `simple-tab-${index}`,
+    id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
   };
 }
 
 export default function AuthenticationPage() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: '100vw', height: '100vh' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'lightgrey', width: '100vw' }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          TabIndicatorProps={{ sx: { backgroundColor: 'lightgray', height: 2 } }}
-          sx={{
-            '& button.Mui-selected': { color: 'lightgray', width: '50%' },
-            'backgroundColor':       '#538dbd',
-            'fontFamily':            'Patrick Hand',
-          }}
-        >
-          <Tab label="Sign In" {...a11yProps(0)} sx={{ fontFamily: 'Patrick Hand', color: 'darkgray', width: '50%', fontSize: '2.5vw' }} />
-          <Tab label="Sign Up" {...a11yProps(1)} sx={{ fontFamily: 'Patrick Hand', color: 'darkgray', width: '50%', fontSize: '2.5vw' }} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0} style={{ backgroundColor: '#538dbd', height: '100%' }}>
-        <SignInRoute setValue={setValue} />
-      </TabPanel>
-      <TabPanel value={value} index={1} style={{ backgroundColor: '#538dbd', height: '100%' }}>
-        <SignUpRoute />
-      </TabPanel>
-    </Box>
+    <div className="authenticationPage">
+      <div
+        className="wrapper-wavy-block"
+        style={{ backgroundImage: `url(${blackManta})` }}
+      >
+        <WavyBlock />
+      </div>
+      <div className="buttonsContainer">
+        <Button>Sign In</Button>
+        <Button>Log In</Button>
+      </div>
+      <div className="socialSignUps">
+        <ButtonIcon icon={<img src={googleIcon} alt="Google" />} />
+        <ButtonIcon icon={<img src={facebookIcon} alt="Facebook" />} />
+        <ButtonIcon icon={<img src={appleIcon} alt="Apple" />} />
+      </div>
+    </div>
+    // <Box sx={{ width: '100vw', height: '100vh' }}>
+    //   <Box sx={{ borderBottom: 1, borderColor: 'lightgrey', width: '100vw' }}>
+    //     <Tabs
+    //       value={value}
+    //       onChange={handleChange}
+    //       TabIndicatorProps={{
+    //         sx: { backgroundColor: 'lightgray', height: 2 },
+    //       }}
+    //       sx={{
+    //         '& button.Mui-selected': { color: 'lightgray', width: '50%' },
+    //         backgroundColor: '#538dbd',
+    //         fontFamily: 'Patrick Hand',
+    //       }}
+    //     >
+    //       <Tab
+    //         label="Sign In"
+    //         {...a11yProps(0)}
+    //         sx={{
+    //           fontFamily: 'Patrick Hand',
+    //           color: 'darkgray',
+    //           width: '50%',
+    //           fontSize: '2.5vw',
+    //         }}
+    //       />
+    //       <Tab
+    //         label="Sign Up"
+    //         {...a11yProps(1)}
+    //         sx={{
+    //           fontFamily: 'Patrick Hand',
+    //           color: 'darkgray',
+    //           width: '50%',
+    //           fontSize: '2.5vw',
+    //         }}
+    //       />
+    //     </Tabs>
+    //   </Box>
+    //   <TabPanel
+    //     value={value}
+    //     index={0}
+    //     style={{ backgroundColor: '#538dbd', height: '100%' }}
+    //   >
+    //     <SignInRoute setValue={setValue} />
+    //   </TabPanel>
+    //   <TabPanel
+    //     value={value}
+    //     index={1}
+    //     style={{ backgroundColor: '#538dbd', height: '100%' }}
+    //   >
+    //     <SignUpRoute />
+    //   </TabPanel>
+    // </Box>
   );
 }
