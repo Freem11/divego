df2c68927ec8654c6b75b8db455474d313a12ae1
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2024-12-03 12:08:55 -0700

    web45: setup left slide and right slide constants

--- wetmap/src/components/reusables/slider/index.tsx
+++ wetmap/src/components/reusables/slider/index.tsx
@@ -1,4 +1,4 @@
-import React, { DetailedHTMLProps, InputHTMLAttributes, useRef, useState } from "react";
+import React, { DetailedHTMLProps, InputHTMLAttributes, useRef, useState } from 'react';
 import './style.scss';
 
 type TextInputProps = DetailedHTMLProps<
@@ -6,21 +6,39 @@ type TextInputProps = DetailedHTMLProps<
   HTMLInputElement
 >;
 type CustomInputProps = {
-  children: React.ReactElement;
-  onFileChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  children:      React.ReactElement
+  onFileChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 };
 
 const Slider = React.forwardRef(function Slider(
   props: TextInputProps & CustomInputProps,
-  ref
+  ref,
 ) {
   const { children, onChange, onFileChange, ...rest } = props;
-  
-const [slideNum, setSlideNum] = useState<number>(1);
+
+  const [prevSlideNum, setPrevSlideNum] = useState<number>(0);
+  const [slideNum, setSlideNum] = useState<number>(1);
+
+  const animationDuration = 300;
+  const pageWidth = '33vw';
+
+  const slideLeftStyle = {
+    // aka page forward
+    transform:  `translateX(${slideNum - prevSlideNum}*${pageWidth})`,
+    transition: `transform ${animationDuration}ms ease-in-out`,
+  };
+
+  const slideRightStyle = {
+    // aka page backward
+    transform:  `translateX(-${slideNum - prevSlideNum}*${pageWidth})`,
+    transition: `transform ${animationDuration}ms ease-out`,
+  };
+
   return (
     <>
-      <div className="slider"><div className="slider-center-container">{children[slideNum]}</div>
-      
+      <div className="slider">
+        <div className="slider-center-container">{children[slideNum]}</div>
+
       </div>
     </>
   );
