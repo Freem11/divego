0e4408c03507a780159ee7bdb35c2a4addf5037d
kbyun-ubuntu-frrqn73 <72177353+k-byun@users.noreply.github.com>
2024-12-16 21:37:11 -0700

    50% of remaining task complete

--- wetmap/src/components/pages/authentication/step1/index.tsx
+++ wetmap/src/components/pages/authentication/step1/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 // import Button from "../../../reusables/button";
 import { SliderContext } from "../../../reusables/slider/context";
 import Icon from "../../../../icons/Icon";
@@ -6,26 +6,36 @@ import { Box, Typography } from "@mui/material";
 import carouselData from "../carousel-data.json";
 import TextInput from "../../../reusables/textInput";
 import { useForm } from "react-hook-form";
-import { Form, FormRules } from './form';
+import { Form, FormRules } from "./form";
 import Button from "../../../reusables/button";
 
 export default function TutorialStep3() {
-  const { register, handleSubmit, formState: { isSubmitting, errors } } = useForm<Form>();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<Form>();
   const { goToSlide } = useContext(SliderContext);
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
 
-  
   const { slideForward } = useContext(SliderContext);
   const onSubmit = (data: Form) => {
-      // slideForward();
-      console.log(data);
-    };
+    // slideForward();
+    console.log(data);
+  };
   return (
     <>
       {/* <Box sx={{ position: 'relative', height: 62, left: "3rem" }} /> */}
       <Box sx={{ height: 62 }} />
 
-      <div style={{ display: "flex", flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'flex-end'}}>
-
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          alignItems: "flex-end",
+        }}
+      >
         <Icon
           name="chevron-right"
           fill="white"
@@ -34,71 +44,91 @@ export default function TutorialStep3() {
         />
       </div>
 
-      <Box sx={{ height: 48 }} />
-
-      <Typography variant="h3" align="left" sx={{ ml: 9 }}>
-        {carouselData[0].title}
-      </Typography>
-
-      <Box sx={{ height: 48 }} />
-
-      <form
-        className="flex-column-between mx-6 mb-6"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <TextInput
-          error={errors.fullname}
-          iconLeft={<Icon name="person" />}
-          placeholder={carouselData[0].namePlaceholder}
-          {...register("fullname", FormRules.fullname)}
-        />
-        <Box sx={{ height: 16 }} />
-        
-        <TextInput
-          error={errors.email}
-          iconLeft={<Icon name="person" />}
-          placeholder={carouselData[0].emailPlaceholder}
-          {...register("email", FormRules.email)}
-        />
-
-        <Box sx={{ height: 16 }} />
-
-        <TextInput
-          error={errors.password}
-          iconLeft={<Icon name="person" />}
-          placeholder={carouselData[0].passwordPlaceholder}
-          {...register("password", FormRules.password)}
-        />
-
-        <div className="mb-4"></div>
+      <Box sx={{ mt: "48px", mx: 9 }}>
+        <Typography variant="h3" align="left">
+          {carouselData[0].title}
+        </Typography>
 
         <Box sx={{ height: 48 }} />
 
-        <Button
-          disabled={isSubmitting}
-          className="btn-lg bg-primary w-25"
-          type="submit"
-          iconRight={<Icon name="chevron-right" />}
+        <form
+          className="flex-column-between mx-6 mb-6"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          {carouselData[0].buttonText}
-        </Button>
+          <div style={{ backgroundColor: "white" }}>
+            <TextInput
+              error={errors.fullname}
+              iconLeft={<Icon name="person" />}
+              placeholder={carouselData[0].namePlaceholder}
+              {...register("fullname", FormRules.fullname)}
+            />
+          </div>
+          <Box sx={{ height: 16 }} />
 
+          <div style={{ backgroundColor: "white" }}>
+            <TextInput
+              error={errors.email}
+              iconLeft={<Icon name="at" />}
+              placeholder={carouselData[0].emailPlaceholder}
+              {...register("email", FormRules.email)}
+            />
+          </div>
 
-        <Box sx={{ height: 48 }} />
+          <Box sx={{ height: 16 }} />
 
-        <p>{carouselData[0].promptText}
-          <span
-            style={{ cursor: 'pointer', color: 'blue', textDecoration: 'none' }}
-            onClick={() => goToSlide(2)}
+          <div style={{ backgroundColor: "white" }}>
+            <TextInput
+              error={errors.password}
+              iconLeft={<Icon name="lock-outline" />}
+              iconRight={
+                secureTextEntry ? (
+                  <Icon name="eye" onClick={() => setSecureTextEntry(false)} />
+                ) : (
+                  <Icon
+                    name="eye-off"
+                    onClick={() => setSecureTextEntry(true)}
+                  />
+                )
+              }
+              placeholder={carouselData[0].passwordPlaceholder}
+              {...register("password", FormRules.password)}
+            />
+          </div>
+
+          <Box
+            sx={{
+              mt: "64px",
+              width: "30%",
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
           >
-            {carouselData[0].promptLinkText}
-          </span>
-        </p>
+            <Button
+              disabled={isSubmitting}
+              className="btn-lg bg-primary w-25"
+              type="submit"
+              iconRight={<Icon name="chevron-right" />}
+            >
+              {carouselData[0].buttonText}
+            </Button>
+          </Box>
+          <Box sx={{ height: 48 }} />
 
-      </form>
-
-     
-        
+          <p>
+            {carouselData[0].promptText}
+            <span
+              style={{
+                cursor: "pointer",
+                color: "blue",
+                textDecoration: "none",
+              }}
+              onClick={() => goToSlide(2)}
+            >
+              {carouselData[0].promptLinkText}
+            </span>
+          </p>
+        </form>
+      </Box>
     </>
   );
 }
--- wetmap/src/components/pages/authentication/step3/index.tsx
+++ wetmap/src/components/pages/authentication/step3/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 // import Button from "../../../reusables/button";
 import { SliderContext } from "../../../reusables/slider/context";
 import Icon from "../../../../icons/Icon";
@@ -6,22 +6,25 @@ import { Box, Typography } from "@mui/material";
 import carouselData from "../carousel-data.json";
 import TextInput from "../../../reusables/textInput";
 import { useForm } from "react-hook-form";
-import { Form, FormRules } from './form';
+import { Form, FormRules } from "./form";
 import Button from "../../../reusables/button";
 
 export default function TutorialStep3() {
-  const { register, handleSubmit, formState: { isSubmitting, errors } } = useForm<Form>();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<Form>();
   const { goToSlide } = useContext(SliderContext);
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
 
-  
   const { slideBackward } = useContext(SliderContext);
   const onSubmit = (data: Form) => {
-      // slideForward();
-      console.log(data);
-    };
+    // slideForward();
+    console.log(data);
+  };
   return (
     <>
-      {/* <Box sx={{ position: 'relative', height: 62, left: "3rem" }} /> */}
       <Box sx={{ height: 62 }} />
 
       <div style={{ display: "flex" }}>
@@ -33,62 +36,82 @@ export default function TutorialStep3() {
         />
       </div>
 
-      <Box sx={{ height: 48 }} />
-
-      <Typography variant="h3" align="left" sx={{ ml: 9 }}>
-        {carouselData[2].title}
-      </Typography>
-
-      <Box sx={{ height: 48 }} />
-
-      <form
-        className="flex-column-between mx-6 mb-6"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <TextInput
-          error={errors.email}
-          iconLeft={<Icon name="person" />}
-          placeholder={carouselData[2].emailPlaceholder}
-          {...register("email", FormRules.username)}
-        />
-        <Box sx={{ height: 16 }} />
-        
-        <TextInput
-          error={errors.password}
-          iconLeft={<Icon name="person" />}
-          placeholder={carouselData[2].passwordPlaceholder}
-          {...register("password", FormRules.username)}
-        />
-
-        <div className="mb-4"></div>
+      <Box sx={{ mt: "48px", mx: 9 }}>
+        <Typography variant="h3" align="left">
+          {carouselData[2].title}
+        </Typography>
 
         <Box sx={{ height: 48 }} />
 
-        <Button
-          disabled={isSubmitting}
-          className="btn-lg bg-primary w-25"
-          type="submit"
-          iconRight={<Icon name="chevron-right" />}
+        <form
+          className="flex-column-between mx-6 mb-6"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          {carouselData[2].buttonText}
-        </Button>
+          <div style={{ backgroundColor: "white" }}>
+            <TextInput
+              error={errors.email}
+              iconLeft={<Icon name="at" />}
+              placeholder={carouselData[2].emailPlaceholder}
+              {...register("email", FormRules.email)}
+            />
+          </div>
 
+          <Box sx={{ height: 16 }} />
 
-        <Box sx={{ height: 48 }} />
+          <div style={{ backgroundColor: "white" }}>
+            <TextInput
+              error={errors.password}
+              iconLeft={<Icon name="lock-outline" />}
+              iconRight={
+                secureTextEntry ? (
+                  <Icon name="eye" onClick={() => setSecureTextEntry(false)} />
+                ) : (
+                  <Icon
+                    name="eye-off"
+                    onClick={() => setSecureTextEntry(true)}
+                  />
+                )
+              }
+              placeholder={carouselData[2].passwordPlaceholder}
+              {...register("password", FormRules.password)}
+            />
+          </div>
 
-        <p>{carouselData[2].promptText}
-          <span
-            style={{ cursor: 'pointer', color: 'blue', textDecoration: 'none' }}
-            onClick={() => goToSlide(0)}
+          <Box
+            sx={{
+              mt: "116px",
+              width: "30%",
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
           >
-            {carouselData[2].promptLinkText}
-          </span>
-        </p>
+            <Button
+              disabled={isSubmitting}
+              className="btn-lg bg-primary"
+              type="submit"
+              iconRight={<Icon name="chevron-right" />}
+            >
+              {carouselData[2].buttonText}
+            </Button>
+          </Box>
 
-      </form>
+          <Box sx={{ height: 48 }} />
 
-     
-        
+          <p>
+            {carouselData[2].promptText}
+            <span
+              style={{
+                cursor: "pointer",
+                color: "blue",
+                textDecoration: "none",
+              }}
+              onClick={() => goToSlide(0)}
+            >
+              {carouselData[2].promptLinkText}
+            </span>
+          </p>
+        </form>
+      </Box>
     </>
   );
 }
