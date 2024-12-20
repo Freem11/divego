4d358cbe100a636ac03bae483d5997117b18182e
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2024-12-20 09:53:03 -0700

    web-27-B-new-layout: added useContext to google map component for zoom buttons

--- wetmap/src/components/googleMap/view.tsx
+++ wetmap/src/components/googleMap/view.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import style from './style.module.scss';
 import { Cluster, HeatPoint, HeatPointConfiguration, LatLngObject, MapConfiguration, SuperclusterInstance } from './types';
