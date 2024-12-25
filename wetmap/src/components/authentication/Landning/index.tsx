4f28919ddc4910c395e0788a383846586ef7d92a
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2024-12-25 07:06:50 -0700

    web-54-Oauth: cleanup up landing page code and removed libraries: react-web-vector-icons,reactjs-social-login, @greatsumini/react-facebook-login, @react-oauth/google, @google-cloud/vision

--- wetmap/src/components/authentication/Landning/index.tsx
+++ wetmap/src/components/authentication/Landning/index.tsx
@@ -1,10 +1,6 @@
 import React, { useContext } from 'react';
 import { SessionContext } from '../../contexts/sessionContext';
 import { SliderContext } from '../../reusables/slider/context';
-import {
-  register,
-  signInStandard,
-} from '../../../supabaseCalls/authenticateSupabaseCalls';
 import { createProfile, grabProfileById } from '../../../supabaseCalls/accountSupabaseCalls';
 import './index.css';
 import LandingPageView from './view';
@@ -16,13 +12,6 @@ const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const facebookAppId = import.meta.env.VITE_FACEBOOK_APP_ID;
 const appleAppId = import.meta.env.VITE_APPLE_APP_ID;
 const REDIRECT_URI = window.location.href;
-
-type formProps = {
-  password:  string
-  email:     string
-  firstName: string
-  lastName:  string
-};
 
 export default function LandingPage() {
   const { goToSlide } = useContext(SliderContext);
@@ -56,34 +45,6 @@ export default function LandingPage() {
     if (data) handleSupabaseSetup(data, setActiveSession);
   }
 
-  const handleOAuthSubmit = async (user: any) => {
-    let Fname;
-    let LName;
-    const Pword = user.id;
-    const MailE = user.email;
-
-    if (user.given_name) {
-      if (user.family_name) {
-        Fname = user.given_name;
-        LName = user.family_name;
-      } else {
-        Fname = user.given_name.split(' ').slice(0, -1).join(' ');
-        LName = user.given_name.split(' ').slice(-1)[0];
-      }
-    } else if (user.name) {
-      Fname = user.name.split(' ').slice(0, 1);
-      LName = user.name.split(' ').slice(-1);
-    }
-
-    await OAuthSignIn({
-      password:  Pword,
-      email:     MailE,
-      firstName: Fname,
-      lastName:  LName,
-    });
-  };
-
-
   async function handleSupabaseSetup(sessionToken: any, setActiveSession: React.Dispatch<React.SetStateAction<ActiveSession | null>>) {
     if (sessionToken) {
       await localStorage.setItem('token', JSON.stringify(sessionToken));
@@ -107,40 +68,6 @@ export default function LandingPage() {
           email: sanitizeData.user.email,
         });
         console.log('profile created!');
-      }
-    }
-  }
-  async function OAuthSignIn(formVals: formProps) {
-    const accessToken = await signInStandard(formVals);
-    if (accessToken?.data.session !== null) {
-      await localStorage.setItem(
-        'token',
-        JSON.stringify(accessToken?.data.session.refresh_token),
-      );
-      if (accessToken) {
-        setActiveSession(accessToken?.data.session);
-      }
-
-      return;
-    } else {
-      const registrationToken = await register(formVals);
-      if (registrationToken) {
-        await createProfile({
-          id:    registrationToken?.data?.session?.user.id,
-          email: registrationToken?.data?.user?.email,
-        });
-      }
-
-      if (registrationToken?.data.session !== null) {
-        await localStorage.setItem(
-          'token',
-          JSON.stringify(registrationToken?.data.session.refresh_token),
-        );
-        if (registrationToken) {
-          setActiveSession(registrationToken?.data.session);
-        }
-      } else {
-        alert('You already have an account with this email');
       }
     }
   }
