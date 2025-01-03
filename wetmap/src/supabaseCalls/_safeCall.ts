63725b8820a1016ab19324875215e5b73b883959
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-02 17:42:59 -0700

    safe call refactor

--- wetmap/src/supabaseCalls/_safeCall.ts
+++ wetmap/src/supabaseCalls/_safeCall.ts
@@ -1,21 +1,27 @@
 import { toast } from 'react-toastify';
 import screenData from '../components/newModals/screenData.json';
 
+class SupabaseError extends Error {
+    constructor(message: string, public details?: string) {
+        super(`${"[Supabase] Error: " + message}`);
+        this.name = "SupabaseError";
+    }
+}
+
 type SupabaseCall<T> = () => Promise<{ data: T | null; error: any }>;
 
-export const safeSupabase = async <T>(
+export const safeCall = async <T>(
     supabaseCall: SupabaseCall<T>,
     successMessage?: string,
-    errorMessage?: string
+    errorMessage: string = screenData.Toast.generalError
 ) => {
 
     try {
         const { data, error } = await supabaseCall();
 
         if (error) {
-            toast.error(errorMessage || screenData.Toast.generalError);
-            console.error('[Supabase] Error: ', error);
-            return null;
+            toast.error(errorMessage);
+            throw new SupabaseError(error.message, error.details);
         }
 
         if (successMessage) {
@@ -23,8 +29,10 @@ export const safeSupabase = async <T>(
         }
 
         return data;
+        
     } catch (err) {
-        toast.error(errorMessage || screenData.Toast.generalError);
+        toast.error(errorMessage);
+        console.error('[safeCall] Error: ', err);
         return null;
     }
 };
