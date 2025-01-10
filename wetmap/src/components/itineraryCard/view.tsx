f73b2b43886fae15ee1611c96c3b27fd865331b1
Maxime Boucher <maxime@maximeboucher.com>
2025-01-10 13:45:52 -0700

    web-33-update-itinerary-component: create new itinerary component

---
+++ wetmap/src/components/itineraryCard/view.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import style from './style.module.scss';
+import ButtonIcon from '../reusables/buttonIcon';
+import Icon from '../../icons/Icon';
+import { animated, SpringValue } from 'react-spring';
+import { ItineraryItem } from '../../entities/itineraryItem';
+
+type TripCardViewProps = {
+  itinerary:              ItineraryItem
+  flipMap:                (siteList: number[]) => Promise<void>
+  startMoreInfoAnimation: (id: number) => void
+  heightChange:           { height: SpringValue<number> }
+};
+
+export default function ItineraryCardView({ itinerary, flipMap, startMoreInfoAnimation, heightChange }: TripCardViewProps) {
+  return (
+    <div className={style.masterBox} key={itinerary.id}>
+      <div className={style.shadowbox}>
+        <div className={style.moreBox}>
+          <p className={style.tripName}>{itinerary.tripName}</p>
+          <p
+            className={style.opener}
+            onClick={() => startMoreInfoAnimation(itinerary.id)}
+          >
+            More Info
+          </p>
+        </div>
+        <div className={style.buttonBox}>
+          <ButtonIcon
+            icon={<Icon name="anchor" />}
+            className={`btn-lg ${style.buttonStyling}`}
+            onClick={() => flipMap(itinerary.siteList)}
+          />
+          <ButtonIcon
+            icon={<Icon name="diving-scuba-flag" />}
+            className={`btn-lg ${style.buttonStyling}`}
+          // onClick={}
+          />
+        </div>
+      </div>
+      <animated.div className={style.extraBox} style={heightChange}>
+        <div className={style.topRail}>
+          <p className={style.dateText}>
+            {itinerary.startDate}
+            {' to '}
+            {itinerary.endDate}
+          </p>
+          <p className={style.priceText}>{itinerary.price }</p>
+        </div>
+
+        <p className={style.lowerText}>{itinerary.description}</p>
+      </animated.div>
+    </div>
+  );
+}
