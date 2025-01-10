303921b05ac159573fb94b462981ada99ff8c035
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-10 15:06:15 -0700

    toast | pictureUpload

--- wetmap/src/components/newModals/picUploader/index.tsx
+++ wetmap/src/components/newModals/picUploader/index.tsx
@@ -6,43 +6,56 @@ import { clearPreviousImage, handleImageUpload } from '../imageUploadHelpers';
 import { insertPhotoWaits } from '../../../supabaseCalls/photoWaitSupabaseCalls';
 import { Form } from './form';
 import { ModalHandleProps } from '../../reusables/modal/types';
+import { toast } from 'react-toastify';
+import screenData from '../screenData.json';
 
 type PicUploaderProps = Partial<ModalHandleProps> & { pictureId?: number };
+
 export default function PicUploader(props: PicUploaderProps) {
   const { pin, setPin } = useContext(PinContext);
   const [picUrl, setPicUrl] = useState<string | null>(null);
 
   props?.registerModalCancelCallback?.(() => {
-    if (pin.PicFile) {
-      clearPreviousImage(pin.PicFile);
+    if (pin?.PicFile) {
+      clearPreviousImage(pin?.PicFile);
       setPin({ ...pin, PicFile: null });
     }
   });
 
   const handleImageSelection = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (pin.PicFile) {
-      clearPreviousImage(pin.PicFile);
+    if (pin?.PicFile) {
+      clearPreviousImage(pin?.PicFile);
     }
-
-    const createFileName = await handleImageUpload(event);
-    setPicUrl(import.meta.env.VITE_CLOUDFLARE_R2_BUCKET_PATH + `${createFileName}`);
-    setPin({
-      ...pin,
-      PicFile: `animalphotos/public/${createFileName}`,
-    });
+    try {
+      const createFileName = await handleImageUpload(event);
+      setPicUrl(import.meta.env.VITE_CLOUDFLARE_R2_BUCKET_PATH + `${createFileName}`);
+      setPin({
+        ...pin,
+        PicFile: `animalphotos/public/${createFileName}`,
+      });
+    } catch (e) {
+      console.error(e);
+      toast.error(screenData.PicUploader.fileUploadError);
+    }
   };
 
-  const onSubmit = async (data: Required<Form>) => {
-    insertPhotoWaits({
-      label:      data.animal.label,
-      dateTaken:  data.date,
-      UserID:     pin.UserID,
-      photoFile:  pin.PicFile,
-      latitude:   pin.Latitude,
-      longitude:  pin.Longitude,
+  const onSubmit = async (formData: Required<Form>) => {
+    const { error } = await insertPhotoWaits({
+      label:      formData.animal.label,
+      dateTaken:  formData.date,
+      UserID:     pin?.UserID,
+      photoFile:  pin?.PicFile,
+      latitude:   pin?.Latitude,
+      longitude:  pin?.Longitude,
     });
+
+    if (error) {
+      toast.error(screenData.PicUploader.uploadError);
+      return;
+    }
+
+    toast.success(screenData.PicUploader.uploadSuccess);
     setPin({ ...pin, PicFile: null });
-    props?.onModalSuccess?.();
   };
 
   return (
@@ -53,7 +66,7 @@ export default function PicUploader(props: PicUploaderProps) {
       onClose={props.onModalCancel}
       onSubmit={onSubmit}
       values={{
-        diveSiteName: pin.siteName,
+        diveSiteName: pin?.siteName,
       }}
     />
   );
