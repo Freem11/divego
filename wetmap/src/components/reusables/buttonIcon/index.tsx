977c350673e4eda8c72373dbdd045231c0d9b8f1
Maxime Boucher <maxime@maximeboucher.com>
2025-01-11 08:01:07 -0700

    web-33-update-itinerary-component: add title attribute to button icons

--- wetmap/src/components/reusables/buttonIcon/index.tsx
+++ wetmap/src/components/reusables/buttonIcon/index.tsx
@@ -5,11 +5,12 @@ export type ButtonProps = {
   onClick?:   (event?: React.MouseEvent<HTMLButtonElement>) => void
   icon?:      React.ReactNode
   className?: string
+  title?:     string
 };
 
 export default function ButtonIcon(props: ButtonProps) {
   return (
-    <button onClick={props.onClick} className={`btn ${style.button} ${props.className ?? ''}`}>
+    <button onClick={props.onClick} className={`btn ${style.button} ${props.className ?? ''}`} title={props.title}>
       <i className={style.icon}>{props.icon}</i>
     </button>
   );
