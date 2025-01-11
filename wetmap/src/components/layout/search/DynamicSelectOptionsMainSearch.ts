66cb59a1906d25eb4ae76fdd8bc092418e2f938f
taras <tymchenko.dev@gmail.com>
2025-01-11 12:55:09 -0700

    web-27 new layout - add types to reusable select

--- wetmap/src/components/layout/search/DynamicSelectOptionsMainSearch.ts
+++ wetmap/src/components/layout/search/DynamicSelectOptionsMainSearch.ts
@@ -1,7 +1,12 @@
 import { DynamicSelectOptions } from '../../reusables/dynamicSelect/DynamicSelectOptions';
 import getPlacePredictions from '../../../helpers/googleMapAutocomplete';
 import { getSiteNamesThatFit } from '../../../supabaseCalls/diveSiteSupabaseCalls';
+import { Option } from '../../reusables/select';
 
+export type data = {
+  id:   string
+  type: string
+};
 
 export class DynamicSelectOptionsMainSearch extends DynamicSelectOptions {
   static getMoreOptions(search: string, limit, skip) {
@@ -9,13 +14,13 @@ export class DynamicSelectOptionsMainSearch extends DynamicSelectOptions {
     const diveSitePromise = getSiteNamesThatFit(search);
 
     return Promise.all([placesPromise, diveSitePromise]).then(([places, diveSites]) => {
-      const options = [];
+      const options: Option<data>[] = [];
       places?.predictions?.forEach((place) => {
         options.push({ key: place.place_id, label: place.description, data: { type: 'place', id: place.place_id } });
       });
 
       diveSites?.forEach((diveSite) => {
-        options.push({ key: diveSite.id, label: diveSite.name, data: { type: 'diveSite', id: diveSite.id } });
+        options.push({ key: diveSite.id.toString(), label: diveSite.name, data: { type: 'diveSite', id: diveSite.id.toString() } });
       });
 
       return { options, totalCount: null };
--- wetmap/src/components/layout/search/index.tsx
+++ wetmap/src/components/layout/search/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { DynamicSelectOptionsMainSearch } from './DynamicSelectOptionsMainSearch';
+import { DynamicSelectOptionsMainSearch, OptionAdditionalData } from './DynamicSelectOptionsMainSearch';
 import DynamicSelect from '../../reusables/dynamicSelect';
 import { DropdownItemProps } from '../../reusables/select/components/dropdownItem';
 import { CoordsContext } from '../../contexts/mapCoordsContext';
@@ -8,19 +8,19 @@ import { SelectedDiveSiteContext } from '../../contexts/selectedDiveSiteContext'
 import { getDiveSitesByIDs } from '../../../supabaseCalls/diveSiteSupabaseCalls';
 import style from './style.module.scss';
 import MainSearchDropdownItem from './components/mainSearchDropdownItem';
+import { onChangeEvent, Option } from '../../reusables/select';
 
 export default function MainSearch() {
   const { setMapCoords } = useContext(CoordsContext);
   const { setSelectedDiveSite } = useContext(SelectedDiveSiteContext);
 
-
-  const handleSelect = async (event) => {
+  const handleSelect = async (event: onChangeEvent<OptionAdditionalData>) => {
     let coordinates: number[] = [];
 
-    const data = event?.target?.value?.data;
+    const option = event?.target?.value as Option<OptionAdditionalData>;
 
-    if (data?.type === 'diveSite') {
-      const diveSites = await getDiveSitesByIDs(JSON.stringify([data.id]));
+    if (option?.data?.type === 'diveSite') {
+      const diveSites = await getDiveSitesByIDs(JSON.stringify([option?.data.id]));
       diveSites?.some((diveSite) => {
         coordinates = [diveSite.lat, diveSite.lng];
         setSelectedDiveSite(diveSite);
@@ -28,8 +28,8 @@ export default function MainSearch() {
       });
     }
 
-    if (data?.type === 'place') {
-      const response = await getPlaceLocation({ placeId: data.id });
+    if (option?.data?.type === 'place') {
+      const response = await getPlaceLocation({ placeId: option?.data.id });
       response?.results?.some((result) => {
         coordinates = [result.geometry.location.lat(), result.geometry.location.lng()];
         return true;
