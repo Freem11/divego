2ab086f6db2cb3aa040165f43b6cca4dd10e630b
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-01-19 18:59:36 -0700

    web-78-depricated-delete-approve: updated email text from divego to scuba seasons

--- wetmap/src/components/newModals/confirmDeleteAccount/index.tsx
+++ wetmap/src/components/newModals/confirmDeleteAccount/index.tsx
@@ -47,7 +47,7 @@ export default function ConfirmDeleteAccount(props: ConfirmDeleteAccountProps) {
 
   const handleOnContact = () => {
     const blurb = `:${activeSession?.user.id}`;
-    const href = `mailto:${screenData.ConfirmDeleteAccount.contantEmail}?subject=Delete%20Account%20Request%20${blurb}&body=Hello%20I%20am%20deleting%20my%20DiveGo%20account%20and%20would%20also%20like%20to%20also%20have%20the%20following%20of%20my%20submissions%20removed%20as%20well%0D%0A%0D%0A%0D%0A%0D%0AMy%20Dive%20Sites%20(Y/N)%0D%0A%0D%0A%0D%0A%0D%0AMy%20Photo%20Submissions%20(Y/N)%0D%0A%0D%0A%0D%0A%0D%0AAs%20removing%20these%20submisions%20would%20diminish%20the%20experience%20for%20others%20divers%20in%20the%20community,%20would%20you%20be%20willing%20to%20negotiate%20with%20DiveGo%20to%20allow%20these%20to%20stay%20in%20the%20app?%20(Y/N)`;
+    const href = `mailto:${screenData.ConfirmDeleteAccount.contantEmail}?subject=Delete%20Account%20Request%20${blurb}&body=Hello%20I%20am%20deleting%20my%20Scube%20SEAsons%20account%20and%20would%20also%20like%20to%20also%20have%20the%20following%20of%20my%20submissions%20removed%20as%20well%0D%0A%0D%0A%0D%0A%0D%0AMy%20Dive%20Sites%20(Y/N)%0D%0A%0D%0A%0D%0A%0D%0AMy%20Photo%20Submissions%20(Y/N)%0D%0A%0D%0A%0D%0A%0D%0AAs%20removing%20these%20submisions%20would%20diminish%20the%20experience%20for%20others%20divers%20in%20the%20community,%20would%20you%20be%20willing%20to%20negotiate%20with%20the%20Scuba%20SEAsons%20team%20to%20allow%20these%20to%20stay%20in%20the%20app?%20(Y/N)`;
     window.location.href = href;
   };
 
