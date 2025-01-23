bb06afc6a128c166d918748565d2e9ba25259243
taras <tymchenko.dev@gmail.com>
2025-01-22 20:01:29 -0700

    web-27 - refactoring

--- wetmap/src/components/googleMap/marker/markerDiveSite/index.tsx
+++ wetmap/src/components/googleMap/marker/markerDiveSite/index.tsx
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import { Marker } from '@react-google-maps/api';
 import icon from '../../../../images/mapIcons/AnchorBlue1.png';
+import iconGold from '../../../../images/mapIcons/AnchorGold.png';
 import { ModalContext } from '../../../reusables/modal/context';
 import DiveSite from '../../../newModals/diveSite';
+import { SitesArrayContext } from '../../../contexts/sitesArrayContext';
 
 type MarkerDiveSiteProps = {
   id:       number
@@ -12,10 +14,11 @@ type MarkerDiveSiteProps = {
 
 export function MarkerDiveSite(props: MarkerDiveSiteProps) {
   const { modalShow } = useContext(ModalContext);
+  const { sitesArray } = useContext(SitesArrayContext);
 
   return (
     <Marker
-      icon={icon}
+      icon={sitesArray.includes(props.id) ? iconGold : icon}
       title={props.title}
       position={props.position}
       onClick={() => {
