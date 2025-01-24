d1d4d1a53fba53bf642c5da2c7166728254b0f53
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-01-24 16:57:44 -0700

    web-85-DiveSite-AddedBy: created supabase query get_single_divesite_byid_info_with_username to pull dive site by id but with username off of profiles table so it could be displayed on the dive site modal

--- wetmap/src/components/newModals/diveSite/view.tsx
+++ wetmap/src/components/newModals/diveSite/view.tsx
@@ -24,6 +24,7 @@ type DiveSiteViewProps = {
 
 export default function DiveSiteView(props: DiveSiteViewProps) {
   const fileUploaderRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="cols mx-0 full-height">
       <input
