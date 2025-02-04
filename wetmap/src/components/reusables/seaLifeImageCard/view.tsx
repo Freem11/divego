50f14bb15bdeecef8343704448f068b612fe30f0
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-02-03 17:48:20 -0700

    web-108-UserProfileSeaLifeImagesAction: added functionality for tag to move to dive site and dispaly it

--- wetmap/src/components/reusables/seaLifeImageCard/view.tsx
+++ wetmap/src/components/reusables/seaLifeImageCard/view.tsx
@@ -13,6 +13,7 @@ type SeaLifeImageCardViewProps = {
   handleModalOpen:     () => void
   handleLike:          (e: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => Promise<void>
   handleProfileSwitch: (e: React.MouseEvent<HTMLHeadingElement, MouseEvent>, username: string) => Promise<void>
+  handleDiveSiteMove:  (lat: number, lng: number) => void
   handleCommentModal:  () => void
   countOfLikes:        number
   picLiked:            boolean
@@ -59,8 +60,14 @@ export default function SeaLifeImageCardView(props: SeaLifeImageCardViewProps) {
               </h4>
             )
           : (
-              <div style={{ height: '24px' }}></div>
+              <h4
+                className={style.userLabel}
+                onClick={() => props.handleDiveSiteMove(props.pic.latitude, props.pic.longitude)}
+              >
+                Go to this location!
+              </h4>
             )}
+
 
         <div className={style.likeButtonContainer}>
           {props.countOfLikes > 0
