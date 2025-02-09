485860cc1c0d5d9720737330cb54ede0f444fb94
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-02-09 07:34:39 -0700

    web-40-CloneTripButton: removed shopCard and replaced with use of ListItemWithImage

--- wetmap/src/components/shopCardList/index.tsx
+++ wetmap/src/components/shopCardList/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import ShopCard from '../shopCard';
 import style from './style.module.scss';
 import { DiveShop } from '../../entities/diveShop';
+import ListItemWithImage from '../reusables/listItemWithImage';
+import getPhotoPublicUrl from '../../helpers/getPhotoPublicUrl';
+import defaultHeaderPicture from '../../images/blackManta.png';
 
 type shopCardListProps = {
   shopList:              DiveShop[] | null
@@ -15,12 +17,20 @@ export default function ShopCardList(props: shopCardListProps) {
   return         (
     <div className={style.shopList}>
       {props.shopList // in the future, if shopList is not empty, render a loading spinner
-      && props.shopList.map((shop: any) => (
-        <ShopCard
-          key={shop.id}
-          shop={shop}
-        />
-      ))}
+      && props.shopList.map((shop: any) => {
+        let imageUrl = defaultHeaderPicture;
+        if (shop.diveShopProfilePhoto) {
+          imageUrl = getPhotoPublicUrl(shop.diveShopProfilePhoto);
+        }
+        return (
+          <ListItemWithImage
+            key={shop.id}
+            title={shop.orgName}
+            date={shop.diveShopBio}
+            imageUrl={imageUrl}
+          />
+        );
+      })}
     </div>
   );
 }
