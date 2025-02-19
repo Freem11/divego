9250678928e14ed2ef0cf879ffd03b235e666bfc
Maxime Boucher <maxime@maximeboucher.com>
2025-02-19 11:07:42 -0700

    web-135-update-sea-life-card: revisions

--- wetmap/src/components/reusables/seaLifeCard/view.tsx
+++ wetmap/src/components/reusables/seaLifeCard/view.tsx
@@ -31,6 +31,11 @@ export default function SeaLifeCardView(props: SeaLifeCardViewProps) {
         src={`https://pub-c089cae46f7047e498ea7f80125058d5.r2.dev/${photoName}`}
         className={`${style.backgroundImage} ${style.fadeDelay}`}
       />
+      {/* Second image serves to get the proper height for the card */}
+      <BlurryImage
+        src={`https://pub-c089cae46f7047e498ea7f80125058d5.r2.dev/${photoName}`}
+        className={style.heightCard}
+      />
       <a
         href={`mailto:scubaseasons@gmail.com?subject=Reporting%20issue%20with%20picture:%20"${props.pic.label}"%20${props.pic.photoFile}&body=Type%20of%20issue:%0D%0A%0D%0A%0D%0A%0D%0A1)%20Animal%20name%20not%20correct%0D%0A%0D%0A(Please%20provide%20correct%20animal%20name%20and%20we%20will%20correct%20the%20record)%0D%0A%0D%0A%0D%0A%0D%0A2)%20Copy%20write%20image%20claim%0D%0A%0D%0A(Please%20provide%20proof%20that%20you%20own%20the%20submitted%20photo%20and%20we%20will%20remove%20it%20as%20you%20have%20requested)`}
         className={style.report}
