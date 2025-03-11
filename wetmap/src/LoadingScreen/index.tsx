9db4119a68ef4d8ddc3fa444aa306b8bd0210fe0
fabf28 <ifavour228@gmail.com>
2025-03-11 17:26:11 -0600

    web-130-loading-screen-ts: primary blue background and css modules

--- wetmap/src/LoadingScreen/index.tsx
+++ wetmap/src/LoadingScreen/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './style.module.css';
+import style from './style.module.scss';
 import MantaRay from '../images/Matt_Manta_White.png';
 
 const LoadingScreen: React.FC = () => {
