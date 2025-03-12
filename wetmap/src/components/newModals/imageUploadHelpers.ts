3fa2880a9ec06aa3a801315c1dd9d6e2f9fff382
fabf28 <ifavour228@gmail.com>
2025-03-11 19:51:21 -0600

    web-120-imageUploadHelpers-ts: figured out photoinfo type issue

--- wetmap/src/components/newModals/imageUploadHelpers.ts
+++ wetmap/src/components/newModals/imageUploadHelpers.ts
@@ -1,47 +1,23 @@
+
 import {
   uploadphoto,
   removePhoto,
 } from '../../cloudflareBucketCalls/cloudflareAWSCalls';
 
-    type handleImageUploadProps = {
-      photoInfo: any
-    };
 
-    type uploadImageProps = {
-      file: File
-      name: string | null
-    };
-
-    type clearPreviousImageProps = {
-      oldFile: string
-    };
-
-const handleImageUpload = async (props: handleImageUploadProps) => {
-  const { photoInfo } = props;
-  let imageFile;
-  if (photoInfo.target && photoInfo.target.files[0]) {
-    imageFile = photoInfo.target.files[0];
-  } else if (photoInfo.name) {
-    imageFile = photoInfo;
+const handleImageUpload = async (files: FileList | null) => {
+  if (!files) {
+    return null;
   }
 
-  const extension = imageFile.name.split('.').pop();
-  const fileName = Date.now() + '.' + extension;
-
-  await uploadImage({ file: imageFile, name: fileName });
-
-  return fileName;
+  const file = files[0];
+  const extension = file.name.split('.').pop();
+  const newName = Date.now() + '.' + extension;
+  await uploadphoto(file, newName);
+  return newName;
 };
 
-const uploadImage = async (props: uploadImageProps) => {
-  const { file, name } = props;
-  const data = new FormData();
-  data.append('image', file);
-  await uploadphoto(file, name);
-};
-
-const clearPreviousImage = async (props: clearPreviousImageProps) => {
-  const { oldFile } = props;
+const clearPreviousImage = async (oldFile: string) => {
   removePhoto({
     filePath: import.meta.env.VITE_CLOUDFLARE_R2_BUCKET_PATH,
     fileName: `${oldFile}`,
