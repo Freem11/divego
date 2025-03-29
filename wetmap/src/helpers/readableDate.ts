65a2a61a8cfca95a67cbb2daa4487201efc19958
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-03-28 19:24:49 -0600

    web-176: added a sethours with 24 to bump the date to the correct one

--- wetmap/src/helpers/readableDate.ts
+++ wetmap/src/helpers/readableDate.ts
@@ -1,8 +1,10 @@
 export default function readableDate(date: string) {
+  const dateObject = new Date(date);
+  dateObject.setHours(24, 0, 0, 0);
   const formattedDate = new Intl.DateTimeFormat('en-US', {
     month: 'short',
     day:   'numeric',
     year:  'numeric',
-  }).format(new Date(date));
+  }).format(new Date(dateObject));
   return formattedDate;
 }
