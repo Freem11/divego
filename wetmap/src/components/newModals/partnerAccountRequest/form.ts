1a8787fa674561f9e9c44d6faefc5df3736e6458
Vladimir Elisavetski <yvova88@yahoo.com>
2025-04-13 15:15:02 -0600

    form partner account

--- wetmap/src/components/newModals/partnerAccountRequest/form.ts
+++ wetmap/src/components/newModals/partnerAccountRequest/form.ts
@@ -1,5 +1,5 @@
-
 import { FormValidationRules } from '../../../forms/form';
+import i18n from '../../../i18n';
 
 export interface Form {
   BusinessName?: string
@@ -8,34 +8,33 @@ export interface Form {
   Longitude?:    number
 }
 
-
 export const FormRules: FormValidationRules<Form> = {
   BusinessName: {
-    required: 'Business name cannot be empty',
+    required: i18n.t('Validators.requiredBusinessName'),
   },
   WebsiteLink: {
-    required: 'Website link cannot be empty',
+    required: i18n.t('Validators.requiredWebsite'),
   },
   Longitude: {
-    required: 'Longitude is required',
+    required: i18n.t('Validators.requiredLongitude'),
     min:      {
       value:   -180,
-      message: 'Longitude must be greater than -180',
+      message: i18n.t('Validators.minLongitude', { value: -180 }),
     },
     max: {
       value:   180,
-      message: 'Longitude must be less than 180',
+      message: i18n.t('Validators.maxLongitude', { value: 180 }),
     },
   },
   Latitude: {
-    required: 'Longitude is required',
+    required: i18n.t('Validators.requiredLatitude'),
     min:      {
       value:   -180,
-      message: 'Latitude must be greater than -180',
+      message: i18n.t('Validators.minLatitude', { value: -180 }),
     },
     max: {
       value:   180,
-      message: 'Latitude must be less than 180',
+      message: i18n.t('Validators.maxLatitude', { value: 180 }),
     },
   },
 };
