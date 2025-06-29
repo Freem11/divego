8ff783cd5c92b4d4be29b0569510a08c590907bb
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-06-28 21:31:53 -0600

    web-158-MapSVG: added anchor and dive centre icon code

--- wetmap/src/components/googleMap/marker/markerDiveShop/index.tsx
+++ wetmap/src/components/googleMap/marker/markerDiveShop/index.tsx
@@ -3,6 +3,7 @@ import { Marker } from '@react-google-maps/api';
 import icon from '../../../../images/mapIcons/DiveCentre24x24.png';
 import { ModalContext } from '../../../reusables/modal/context';
 import ShopModal from '../../../newModals/shopModal';
+import iconConfig from '../../../../icons/_config.json';
 
 type MarkerDiveShopProps = {
   id:       number
@@ -12,10 +13,15 @@ type MarkerDiveShopProps = {
 
 export function MarkerDiveShop(props: MarkerDiveShopProps) {
   const { modalShow } = useContext(ModalContext);
+  const svg = `<svg width="30" height="30" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+  <circle fill="#0073E6" cx="256" cy="256" r="256"/>
+  <path style="transform: scale(0.85); transform-origin: center" fill="white" d="${iconConfig['dive-centre'][1]}"/>
+  </svg>`;
+  const url = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svg);
 
   return (
     <Marker
-      icon={icon}
+      icon={{ url }}
       title={props.title}
       position={props.position}
       onClick={() => {
--- wetmap/src/components/googleMap/marker/markerDiveSite/index.tsx
+++ wetmap/src/components/googleMap/marker/markerDiveSite/index.tsx
@@ -6,6 +6,7 @@ import { ModalContext } from '../../../reusables/modal/context';
 import DiveSite from '../../../newModals/diveSite';
 import { SitesArrayContext } from '../../../contexts/sitesArrayContext';
 import { MapContext } from '../../mapContext';
+import iconConfig from '../../../../icons/_config.json';
 
 type MarkerDiveSiteProps = {
   id:       number
@@ -17,6 +18,18 @@ export function MarkerDiveSite(props: MarkerDiveSiteProps) {
   const { modalShow } = useContext(ModalContext);
   const { sitesArray, setSitesArray } = useContext(SitesArrayContext);
   const { mapConfig } = useContext(MapContext);
+
+  const svg1 = `<svg width="30" height="30" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+  <path style="transform: scale(0.85); transform-origin: center" fill="skyblue" d="${iconConfig.anchor[1]}"/>
+  </svg>`;
+  const url1 = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svg1);
+
+  const svg2 = `<svg width="30" height="30" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+  <path style="transform: scale(0.85); transform-origin: center" fill="gold" d="${iconConfig.anchor[1]}"/>
+  </svg>`;
+  const url2 = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svg2);
+
+  const url = sitesArray.includes(props.id) ? url2 : url1;
 
   function handleClick() {
     if (mapConfig !== 3) {
@@ -35,7 +48,7 @@ export function MarkerDiveSite(props: MarkerDiveSiteProps) {
 
   return (
     <Marker
-      icon={sitesArray.includes(props.id) ? iconGold : icon}
+      icon={url}
       title={props.title}
       position={props.position}
       onClick={handleClick}
